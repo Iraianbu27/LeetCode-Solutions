5edd3c0cbf6122b483b9418ec9e8d63f92f8e1b1
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-08-12 23:23:12 +0530

    Time: 2 ms (99.81%), Space: 43.3 MB (52.97%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,99 @@
+import java.util.*;
+class Solution {
+    public static void  recSolution(List<Integer> list, List<List<Integer>> subList,int[] arr,int target ,int i)
+    {
+        // if(i == arr.length)
+        // {
+            if(target == 0)
+            {
+                subList.add(new ArrayList<>(list)); 
+                return ;      
+            }       
+                 
+        // }
+        for(int k = i ;k<arr.length;k++)
+        {
+            if(k>i && arr[k] == arr[k-1] ) continue;
+            if(arr[k] > target) break;
+            
+            list.add(arr[k]);
+            recSolution(list,subList,arr,target - arr[k]  ,k + 1);
+            list.remove(list.size() - 1);
+            
+        }   
+        // recSolution(list,subList,arr,target,i + 1);
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> subList = new ArrayList<>();
+        Arrays.sort(candidates);
+        recSolution(list,subList,candidates,target ,0);
+        return subList;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// USING SET METHOD
+
+// import java.util.*;
+// class Solution {
+//     public static void  recSolution(List<Integer> list, Set<List<Integer>> subList,int[] arr,int target ,int i)
+//     {
+//         //baseCondition
+//         // if(target < arr[i])
+//         // {
+//         //     return ;
+//         // }
+//         if(i == arr.length)
+//         {
+//             if(target == 0)
+//             {
+//                 subList.add(new ArrayList<>(list));
+//                 // return ;
+//             }       
+//             return ;       
+//         }
+
+//         if(arr[i] <= target) 
+//         {
+//         list.add(arr[i]);
+//         // target = target - arr[i];
+    
+//         recSolution(list,subList,arr,target - arr[i]  ,i +1);
+//         list.remove(list.size() - 1);
+//         // target += arr[i];
+//         }
+
+//         recSolution(list,subList,arr,target,i + 1);
+//     }
+//     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+//         List<Integer> list = new ArrayList<>();
+//         Set<List<Integer>> subList = new LinkedHashSet<>();
+//         Arrays.sort(candidates);
+//         recSolution(list,subList,candidates,target ,0);
+//         List<List<Integer>> finalList = new ArrayList<>(subList);
+
+//         return finalList;
+//     }
+// }
