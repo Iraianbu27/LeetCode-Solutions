6e8f2a5ea3e3aced003a25a095f0a373f22eaa70
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-08-14 12:17:57 +0530

    Time: 2 ms (81.68%), Space: 42.2 MB (64.57%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
@@ -1,27 +1,20 @@
 class Solution {
-    private List<String> sameDigitNumbers = List.of("999", "888", "777", "666", "555", "444", "333", "222", "111", "000");
-
-    // Check whether the 'num' string contains the 'sameDigitNumber' string or not.
-    private boolean contains(String sameDigitNumber, String num) {
-        for (int index = 0; index <= num.length() - 3; ++index) {
-            if (num.charAt(index) == sameDigitNumber.charAt(0) &&
-                num.charAt(index + 1) == sameDigitNumber.charAt(1) &&
-                num.charAt(index + 2) == sameDigitNumber.charAt(2)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public String largestGoodInteger(String num) {
-        // Iterate on all 'sameDigitNumbers' and check if the string 'num' contains it.
-        for (String sameDigitNumber : sameDigitNumbers) {
-            if (contains(sameDigitNumber, num)) {
-                // Return the current 'sameDigitNumbers'.
-                return sameDigitNumber;
+        char maxDigit = 0;
+
+        // Sliding window of size 3
+        for (int i = 0; i <= num.length() - 3; i++) {
+            if (num.charAt(i) == num.charAt(i + 1) &&
+                num.charAt(i) == num.charAt(i + 2)) {
+                if (num.charAt(i) > maxDigit) {
+                    maxDigit = num.charAt(i);
+                }
             }
         }
-        // No 3 consecutive same digits are present in the string 'num'.
-        return "";
+
+        if (maxDigit == 0) {
+            return "";
+        }
+        return String.valueOf(maxDigit).repeat(3);
     }
 }
