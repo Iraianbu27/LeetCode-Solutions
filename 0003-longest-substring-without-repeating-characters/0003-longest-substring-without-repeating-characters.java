c7bf23e28054485f4400ebccb2573b874b33a63b
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-08-18 19:33:13 +0530

    Time: 6 ms (73.3%), Space: 44.8 MB (57.76%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,22 +1,51 @@
 import java.util.*;
-
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Map<Character,Integer> map = new HashMap<>();
-        int l = 0, max = 0;
 
-        for(int r = 0; r < s.length(); r++){   
+        Map<Character,Integer> map = new HashMap<>();
+        int l=0,max=0;
+        int n = s.length();
+        for(int r=0;r<n;r++){   
             if(map.containsKey(s.charAt(r))){
-                // shift l only if it's behind the duplicate
-                l = Math.max(l, map.get(s.charAt(r)) + 1);
+                 //updated the index of dup ele
+                if(l <= map.get(s.charAt(r)))
+                {
+                    l = map.get(s.charAt(r)) + 1;
+                }
+                // map.put(s.charAt(r),r);
+
             }
 
-            // update the character’s latest index
-            map.put(s.charAt(r), r);
 
-            // check max window size
-            max = Math.max(max, r - l + 1);
+            
+                map.put(s.charAt(r),r);
+            
+            //adding element with index on map
+            if(max < (r-l+1)){ //finding max length
+                max = r-l+1;
+            }
         }
-        return max;
-    }
-}
+return max;
+    }}
+        // FIRST ATTEMPT
+        // Set<Character> set = new HashSet<>();
+        // int l = 0;
+        // int max = 0;
+        // int n = s.length();
+        // for(int r = 0;r<n;r++)
+        // {
+        //     if(set.contains(s.charAt(r)))
+        //     {
+        //         while(l<r && set.contains(s.charAt(r))){ //bring l next to the duplicate elemnt index
+        //             set.remove(s.charAt(l));
+        //             l++;
+        //         }
+        //     }
+        //     set.add(s.charAt(r)); //adding ele to set
+        //     if(max < (r-l+1)){
+        //         max = r-l+1;
+        //     }
+        // }
+        // return max; 
+ 
+ 
