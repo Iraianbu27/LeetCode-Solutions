0548a924ad230817e6a613d4bc6297d8e64b95ce
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-08-22 07:23:59 +0530

    Time: 6 ms (73.25%), Space: 45.1 MB (18.28%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,32 +1,52 @@
 import java.util.*;
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-
+    public int lengthOfLongestSubstring(String s) 
+    {
         Map<Character,Integer> map = new HashMap<>();
-        int l=0,max=0;
-        int n = s.length();
-        for(int r=0;r<n;r++){   
-            if(map.containsKey(s.charAt(r))){
-                 //updated the index of dup ele
-                if(l <= map.get(s.charAt(r)))
-                {
-                    l = map.get(s.charAt(r)) + 1;
-                }
-                // map.put(s.charAt(r),r);
-
-            }
-
-
+        int l=0;
+        int count = 0;
+        for(int r = 0;r<s.length();r++)
+        {
             
-                map.put(s.charAt(r),r);
-            
-            //adding element with index on map
-            if(max < (r-l+1)){ //finding max length
-                max = r-l+1;
+            while(map.containsKey(s.charAt(r)))
+            {
+             char firstChar = s.charAt(l);
+             map.remove(firstChar);
+             l++;
             }
+            map.put(s.charAt(r),r);
+            count = Math.max(count,r-l+1);
         }
-return max;
-    }}
+        return count;
+    }
+}
+
+
+    // SECOND ATTEMPT
+    //Map<Character,Integer> map = new HashMap<>();
+//         int l=0,max=0;
+//         int n = s.length();
+//         for(int r=0;r<n;r++){   
+//             if(map.containsKey(s.charAt(r))){
+//                  //updated the index of dup ele
+//                 if(l <= map.get(s.charAt(r)))
+//                 {
+//                     l = map.get(s.charAt(r)) + 1;
+//                 }
+//                 // map.put(s.charAt(r),r);
+    
+//             }
+
+
+            
+//                 map.put(s.charAt(r),r);
+            
+//             //adding element with index on map
+//             if(max < (r-l+1)){ //finding max length
+//                 max = r-l+1;
+//             }
+//         }
+// return max;
         // FIRST ATTEMPT
         // Set<Character> set = new HashSet<>();
         // int l = 0;
