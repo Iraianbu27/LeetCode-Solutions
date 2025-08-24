b2af7c93967cded0da25b47bd492dc457a9b405b
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-08-24 08:45:55 +0530

    Time: 19 ms (55.19%), Space: 58.4 MB (48.76%) - LeetHub

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        int maxBloom = 0;
+        int minBloom =  Integer.MAX_VALUE;
+        if((long)(m*k) > (bloomDay.length)) return -1;
+        for(int i:bloomDay)
+        {
+            maxBloom = Math.max(maxBloom,i);
+            minBloom = Math.min(minBloom,i);
+
+        }
+        // if((m*k) > bloomDay.length) return -1;
+       
+ 
+        int low =minBloom,high = maxBloom;
+        int mid = 0;
+        int result = -1;
+        while(low<=high)
+        {
+            mid = (high+low)/2;
+            if(func(bloomDay,maxBloom,minBloom,mid,m,k)){
+                result = mid;
+                high = mid -1;
+            }
+            else{
+                low = mid +1;
+            }
+
+        }
+        return result;
+    }
+    public boolean func(int[] bloomDay,int maxBloom,int minBloom,int i,int m,int k)
+    {
+            int tot =0;
+            int countFlower = 0;
+            for(int j = 0;j<bloomDay.length;j++)
+            {
+                if(bloomDay[j] <= i)
+                {
+                    countFlower = countFlower + 1;         
+                }
+                else{
+                    tot = tot + (int)(countFlower/k);
+                    countFlower = 0;
+                }
+                
+            }
+            tot = tot + (int)(countFlower/k);
+            if(tot >= m)
+            {
+                return true;
+            }
+        
+        return false;
+           
+    }
+}
