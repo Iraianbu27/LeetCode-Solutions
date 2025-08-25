317f5e2dae413bf619bff5ef8de8c35fcd1fe960
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-08-25 10:29:51 +0530

    Time: 0 ms (100%), Space: 43.2 MB (25.97%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -2,24 +2,40 @@ import java.util.*;
 
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int missingCount = 0;  // counts missing numbers
-        int current = 1;       // start checking from 1
-        int i = 0;             // pointer for arr
-
-        while (missingCount < k) {
-            // if current is in arr, move arr pointer
-            if (i < arr.length && arr[i] == current) {
-                i++;
-            } else {
-                // current is missing
-                missingCount++;
-                if (missingCount == k) {
-                    return current;
-                }
+        int n = arr.length;
+        for(int i = 0;i<n;i++)
+        {
+            if(arr[i] > k)
+            {
+                return k;
             }
-            current++;
+            else{
+                k++;
+            }
         }
-
-        return -1; // should never reach here
+        if(k != 0)
+        {
+            return k;
+        }
+        return 0;
     }
 }
+        // int missingCount = 0;  // counts missing numbers
+        // int current = 1;       // start checking from 1
+        // int i = 0;             // pointer for arr
+
+        // while (missingCount < k) {
+        //     // if current is in arr, move arr pointer
+        //     if (i < arr.length && arr[i] == current) {
+        //         i++;
+        //     } else {
+        //         // current is missing
+        //         missingCount++;
+        //         if (missingCount == k) {
+        //             return current;
+        //         }
+        //     }
+        //     current++;
+        // }
+
+        // return -1; // should never reach here
