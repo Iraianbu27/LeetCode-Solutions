291d615f2c5da96917d40ab0b34d589c2f9c05df
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-08-25 11:11:15 +0530

    Time: 0 ms (100%), Space: 42.5 MB (94.81%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -2,17 +2,42 @@ import java.util.*;
 
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int n = arr.length;
-        for(int i = 0;i<n;i++)
+     int low = 0, high=arr.length-1;
+     int mid = 0;
+     int missingValue = 0;
+     while(low <= high)
+     {
+        mid = (low + high)/2;
+        missingValue = arr[mid] - (mid+1); //actual value that should be at mid index
+        // if actual value is lesser than needed(k) mid+1;
+        if(missingValue < k)
         {
-            if(arr[i] <= k)
-            {
-                k++;
-            }
-            else{
-                break;
-            }
+            low = mid + 1;
+        }
+        else if(missingValue >= k){
+            high = mid - 1;
+        }
+     }
+     return (low+k);
+    }
+}
+
+// OPTIMAL SOLUTION
+//  int n = arr.length;
+    //     for(int i = 0;i<n;i++)
+    //     {
+    //         if(arr[i] <= k)
+    //         {
+    //             k++;
+    //         }
+    //         else{
+    //             break;
+    //         }
             
+
+    // }
+    // return k;
+  
         // BRUTE FORCE WITH 0MS      
         //     if(arr[i] > k)
         //     {
@@ -27,10 +52,8 @@ class Solution {
         //     return k;
         // }
         // return 0;
-    }
-    return k;
-    }
-}
+
+        // approach 2 FAILED    
         // int missingCount = 0;  // counts missing numbers
         // int current = 1;       // start checking from 1
         // int i = 0;             // pointer for arr
