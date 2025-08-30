eb5f20ca1e1a990f273da7e68c679daaa908d2c1
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-08-30 08:01:04 +0530

    Time: 4 ms (50.23%), Space: 53.5 MB (21.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,26 @@
 import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer,Integer> mpp = new TreeMap<>();
-        for(int i: nums)
-        {
-            mpp.put(i,mpp.getOrDefault(i,1)+1);
-        }
-        int max = 0;
-        int key = 0;
-        for(Map.Entry<Integer,Integer> entry : mpp.entrySet())
-        {
-            if(max < entry.getValue())
-            {
-                max = entry.getValue();
-                key = entry.getKey();
-            }
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n/2];
+        //Hash map method
+        // Map<Integer,Integer> mpp = new TreeMap<>();
+        // for(int i: nums)
+        // {
+        //     mpp.put(i,mpp.getOrDefault(i,0)+1);
+        // }
+        // int max = 0;
+        // int key = 0;
+        // for(Map.Entry<Integer,Integer> entry : mpp.entrySet())
+        // {
+        //     if(max < entry.getValue())
+        //     {
+        //         max = entry.getValue();
+        //         key = entry.getKey();
+        //     }
 
-        }
-        return key;
+        // }
+        // return key;
     }
 }
