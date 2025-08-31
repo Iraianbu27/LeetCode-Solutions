7d09afdb7d250a176e8e3e3369248ef0cca3aedb
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-08-31 07:58:24 +0530

    Time: 31 ms (56.48%), Space: 67.9 MB (22.15%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,29 +1,60 @@
 import java.util.*;
 class Solution {
     public int longestConsecutive(int[] nums) {
-        Arrays.sort(nums);
-        int count=1;
-        int maxCount = 0;
-        if(nums.length <= 0)
+        if(nums.length == 0)
         {
             return 0;
         }
-
-        for(int i = 0;i<nums.length-1;i++)
+       Set<Integer> set = new HashSet<>();
+       for(int i : nums)
+       {
+        set.add(i);
+       }
+       int maxLength = 0;
+       
+    //    int currNum = 0;
+        for(int i : set)
         {
-            if(nums[i+1] == nums[i]) continue;
-            if((nums[i+1] - nums[i]) != 1)
+            int count = 0;
+            if(!(set.contains(i-1)))
             {
-                maxCount = Math.max(maxCount,count);
-                count = 1;
-            }
-            
-            else{
                 count++;
+                int currNum = i;
+                while(set.contains(currNum+1))
+                {
+                    count++;
+                    currNum = currNum + 1;
+                }
+                maxLength = Math.max(maxLength, count);
             }
         }
-        maxCount = Math.max(maxCount,count);
-        return maxCount;
+        return maxLength;
+       
+   
         
     }
 }
+ //    its O(N log N) N for looping log N due to sorting array
+        // Arrays.sort(nums);
+        // int count=1;
+        // int maxCount = 0;
+        // if(nums.length <= 0)
+        // {
+        //     return 0;
+        // }
+
+        // for(int i = 0;i<nums.length-1;i++)
+        // {
+        //     if(nums[i+1] == nums[i]) continue;
+        //     if((nums[i+1] - nums[i]) != 1)
+        //     {
+        //         maxCount = Math.max(maxCount,count);
+        //         count = 1;
+        //     }
+            
+        //     else{
+        //         count++;
+        //     }
+        // }
+        // maxCount = Math.max(maxCount,count);
+        // return maxCount;
