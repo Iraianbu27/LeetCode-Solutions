8827bdc26e34dd399ed2b55f3199de8a7b9a2d46
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-01 21:59:16 +0530

    Time: 3 ms (55.5%), Space: 53.8 MB (8.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,9 +1,38 @@
 import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        return nums[n/2];
+       int count = 0;
+       int el = 0;
+       for(int i = 0;i<nums.length;i++)
+       {
+        if(count == 0)
+        {
+            el = nums[i];
+            count++;
+        }
+        else if(nums[i] == el)
+        {
+            count++;
+        }
+        else  
+        {
+            count--;
+        }
+       }
+       int finalCount = 0;
+       for(int i = 0;i<nums.length;i++)
+       {
+        if(nums[i] == el) finalCount++;
+       }
+       if(finalCount > (nums.length/2)) return el;
+       return 0;
+    }
+}
+ // Arrays.sort(nums);
+        // int n = nums.length;
+        // return nums[n/2];
+
+        //approach 2
         //Hash map method
         // Map<Integer,Integer> mpp = new TreeMap<>();
         // for(int i: nums)
@@ -22,5 +51,3 @@ class Solution {
 
         // }
         // return key;
-    }
-}
