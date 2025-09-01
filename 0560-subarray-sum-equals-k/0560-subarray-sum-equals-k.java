76a3f3ca6360247111868596da34c94aa3caac20
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-01 10:24:45 +0530

    Time: 23 ms (92.65%), Space: 46.9 MB (19.22%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,20 +1,43 @@
+import java.util.*;
 class Solution {
-    public int subarraySum(int[] nums, int k) {
-      
-      int subArr = 0;
-      for(int i = 0;i<nums.length;i++)
-      {
-        int count = 0;
-        for(int j = i;j<nums.length;j++)
-        {
-            count += nums[j];
-            if(count == k)
-            {
-                subArr++;
-                // break;
+    //x is prefix x-k ,k
+     public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> prefixCount = new HashMap<>();
+        prefixCount.put(0, 1); // Empty prefix with count 1
+        
+        int prefix = 0;
+        int result = 0;
+        
+        for(int num : nums) {
+            prefix += num;
+            
+            int target = prefix - k;
+            
+            // Add ALL occurrences of target prefix
+            if(prefixCount.containsKey(target)) {
+                result += prefixCount.get(target);
             }
+            
+            // Update count of current prefix
+            prefixCount.put(prefix, prefixCount.getOrDefault(prefix, 0) + 1);
         }
-      }
-      return subArr;
+        
+        return result;
     }
 }
+//BRUTE FORCE O(N^2)
+    //   int subArr = 0;
+    //   for(int i = 0;i<nums.length;i++)
+    //   {
+    //     int count = 0;
+    //     for(int j = i;j<nums.length;j++)
+    //     {
+    //         count += nums[j];
+    //         if(count == k)
+    //         {
+    //             subArr++;
+    //             // break;
+    //         }
+    //     }
+    //   }
+    //   return subArr;
