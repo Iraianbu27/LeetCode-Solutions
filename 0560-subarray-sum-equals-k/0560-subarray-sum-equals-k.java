bfe95c30ce3214321ee0fc215ded5105222d6be7
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-01 10:38:28 +0530

    Time: 24 ms (72.26%), Space: 46.3 MB (76.33%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,28 +1,33 @@
 import java.util.*;
 class Solution {
     //x is prefix x-k ,k
-     public int subarraySum(int[] nums, int k) {
-        HashMap<Integer, Integer> prefixCount = new HashMap<>();
-        prefixCount.put(0, 1); // Empty prefix with count 1
-        
-        int prefix = 0;
-        int result = 0;
-        
-        for(int num : nums) {
-            prefix += num;
+    public int subarraySum(int[] nums, int k) {
+        int prefix =0;
+        int subArr = 0; 
+        Map<Integer,Integer> mpp = new HashMap<>();
+        mpp.put(0, 1);
+
+        for(int i = 0;i<nums.length;i++)
+        {
+            //1.add prefix
+            //2.use formula x-k,k
+            //3.push to ds
             
-            int target = prefix - k;
-            
-            // Add ALL occurrences of target prefix
-            if(prefixCount.containsKey(target)) {
-                result += prefixCount.get(target);
+            prefix += nums[i];
+ 
+            int isPre = prefix - k;
+            if(mpp.containsKey(isPre))
+            {
+                subArr += mpp.get(isPre);
             }
+          
             
-            // Update count of current prefix
-            prefixCount.put(prefix, prefixCount.getOrDefault(prefix, 0) + 1);
+            mpp.put(prefix,mpp.getOrDefault(prefix,0) + 1);
+
         }
-        
-        return result;
+        return subArr;
+
+
     }
 }
 //BRUTE FORCE O(N^2)
