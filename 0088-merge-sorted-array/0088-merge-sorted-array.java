269c76feddea33e1dd166c8b87b5e4f481e24a4d
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-03 11:15:20 +0530

    Time: 0 ms (100%), Space: 42.4 MB (27.36%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,33 +1,37 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] nums = new int[m + n];
-        
-        int left = 0;
-        int right = 0;
-        int index = 0;
-
-        // Merge both arrays
-        while (left < m && right < n) {
-            if (nums1[left] < nums2[right]) {
-                nums[index++] = nums1[left++];
-            } else {
-                nums[index++] = nums2[right++];
+    public void merge(int[] nums1, int m, int[] nums2, int n) {       
+        int i = 0,j=0,index = 0;
+        int[] arr = new int[m+n];
+        while(i< m && j < n)
+        {
+            if(nums1[i] <= nums2[j])
+            {
+                arr[index++] = nums1[i++];
+            }
+            else{
+                arr[index++] = nums2[j++];
             }
         }
-
-        // Copy remaining from nums1
-        while (left < m) {
-            nums[index++] = nums1[left++];
+        while(i<m)
+        {
+            arr[index++] = nums1[i++];
+        }
+        while(j<n)
+        {
+            arr[index++] = nums2[j++];
+        }
+        for(int o = 0;o<m+n;o++)
+        {
+            nums1[o]=arr[o];
         }
 
-        // Copy remaining from nums2
-        while (right < n) {
-            nums[index++] = nums2[right++];
-        }
-
-        // Copy merged array back into nums1
-        for (int i = 0; i < m + n; i++) {
-            nums1[i] = nums[i];
-        }
     }
 }
+// brute force (O(n log n + 3)) ~ O(n log n)
+// for(int i = m;i<nums1.length;i++)
+//         {
+//             if(n == 0) break;
+//             nums1[i] = nums2[j];
+//             j++;
+//         }
+//         Arrays.sort(nums1);
