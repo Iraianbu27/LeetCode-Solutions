93f37cbb08e170476e75663156a8a856f4e3149f
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-04 08:14:09 +0530

    Time: 0 ms (100%), Space: 43.2 MB (57.17%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,61 +1,45 @@
-    class Solution {
-        public void nextPermutation(int[] nums) {
-            int breakPoint = -1;
-            int n = nums.length ;
-            //1.find the break point
-            for(int i = n-2;i>=0;i--)
-            {
-                if(nums[i] < nums[i+1])
-                {
-                    breakPoint = i;
-                    break;
-                }
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int ind1=-1;
+        int ind2=-1;
+        // step 1 find breaking point 
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind1=i;
+                break;
             }
-            if(breakPoint == -1) //input = 5,4,3,2,1 -> no next premutaion so return the first permutation -> 1 2 3 4 5
-            {
-                reverseArr(nums);
-                return ;
-            }
-
-            //2.find the greatest least element of break point in breakPoint+1 to n-1 in //nums
-            for(int i = n-1;i>breakPoint;i--)
-            {
-                if(nums[i] > nums[breakPoint])
-                {
-                    int thVar = nums[breakPoint];
-                    nums[breakPoint] = nums[i];
-                    nums[i] = thVar;
-                    break;
-                }
-                
-            }
-
-            //3. sort the subarray starting from next of breakPoint
-            // for(int i = breakPoint;i<n-1;i++)
-            // {
-            //     if(nums[i] > nums[i+1])
-            //     {
-            //         int thVar = nums[i+1];
-            //         nums[i+1] = nums[i];
-            //         nums[i] = thVar;
-            //     }
-            // }
-            Arrays.sort(nums, breakPoint+1, n);
-                    // 4. reverse the suffix
-        // reverseArr(nums, breakPoint + 1, n - 1);
-            // return nums;
         }
-        public void reverseArr(int[] nums)
-        {
-            int i = 0,j=nums.length -1;
-            while(i<=j)
-            {
-                int thVar = nums[i];
-                nums[i] = nums[j];
-                nums[j] = thVar;
-                i++;
-                j--;
+        // if there is no breaking  point 
+        if(ind1==-1){
+            reverse(nums,0);
+        }
+        
+        else{
+            // step 2 find next greater element and swap with ind2
+            for(int i=nums.length-1;i>=0;i--){
+                if(nums[i]>nums[ind1]){
+                    ind2=i;
+                    break;
+                }
             }
-            // return nums;
+
+            swap(nums,ind1,ind2);
+            // step 3 reverse the rest right half
+            reverse(nums,ind1+1);
         }
     }
+    void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    void reverse(int[] nums,int start){
+        int i=start;
+        int j=nums.length-1;
+        while(i<j){
+            swap(nums,i,j);
+            i++;
+            j--;
+        }
+    }
+}
