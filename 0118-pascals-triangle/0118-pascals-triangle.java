f16bdec5e64573745a5d22893f851a02a5ffcba5
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-04 07:03:02 +0530

    Time: 0 ms (100%), Space: 42.1 MB (40.44%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,28 +1,50 @@
 import java.util.*;
 class Solution {
+    public List generateCol(int row)
+    {
+        List<Integer> rowMatrix = new ArrayList<>();
+        rowMatrix.add(1);
+        int ans = 1;
+        for(int col = 1;col<row;col++)
+        {
+            ans = ans * (row - col);
+            ans = ans/col;
+            rowMatrix.add(ans);
+        }
+        return rowMatrix;
+
+    }
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> matrix = new ArrayList<>();
-        
-        for(int i = 0;i<numRows;i++)
+        for(int row = 1;row<=numRows;row++)
         {
-            List<Integer> arr = new ArrayList<>();
-            for(int j = 0;j<=i;j++)
-            {
-                if(i == j)
-                {
-                    arr.add(1);
-                }
-                else if(j == 0 && i != 0)
-                {
-                    arr.add(1);
-                }
-                else{
-                    arr.add((matrix.get(i-1)).get(j-1) + (matrix.get(i-1)).get(j));
-                }
-                
-            }
-            matrix.add(new ArrayList<>(arr));
+            List rowAns = generateCol(row);
+            matrix.add(rowAns);
         }
-        return matrix;
+     return matrix;
     }
 }
+//brute force
+     // List<List<Integer>> matrix = new ArrayList<>();
+        
+        // for(int i = 0;i<numRows;i++)
+        // {
+        //     List<Integer> arr = new ArrayList<>();
+        //     for(int j = 0;j<=i;j++)
+        //     {
+        //         if(i == j)
+        //         {
+        //             arr.add(1);
+        //         }
+        //         else if(j == 0 && i != 0)
+        //         {
+        //             arr.add(1);
+        //         }
+        //         else{
+        //             arr.add((matrix.get(i-1)).get(j-1) + (matrix.get(i-1)).get(j));
+        //         }
+                
+        //     }
+        //     matrix.add(new ArrayList<>(arr));
+        // }
+        // return matrix;
