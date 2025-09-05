edfea70c1ee3f03f4b2d9d24cb9f36a638855460
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-05 19:50:52 +0530

    Time: 0 ms (100%), Space: 41.9 MB (44.95%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,39 +10,70 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int lengthHead = length( head);
-        ListNode temp = head;
-        int index = lengthHead - n;
-        int count = 0;
-        if(head == null) return head;
-        //remove at head
-        if(index == 0)
+        ListNode fast = head;
+        ListNode slow = head;
+        if(head == null ) return head;
+        if(head.next == null && n == 1) 
+        {
+            return null;
+        }
+        
+        
+        for(int i = 0;i<n;i++)
+        {
+            fast = fast.next;
+        }
+        //remove head
+        if(fast == null)
         {
             return head.next;
         }
-        while(temp != null)
+ 
+        while(fast != null && fast.next != null)
         {
-            if(count + 1 == index)
-            {
-                 
-                    temp.next = temp.next.next;
-                    break;
-            }
-            count++;
-            temp = temp.next;
+            slow = slow.next;
+            fast = fast.next;
         }
+        slow.next = slow.next.next;
         return head;
-
-    }
-    public static int length(ListNode head)
-    {
-        int count = 0;
-        ListNode temp = head;
-        while(temp != null)
-        {
-            count++;
-            temp = temp.next;
-        }
-        return count;
     }
 }
+//BRUTE FORCE USING TWO PASS
+// class Solution {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         int lengthHead = length( head);
+//         ListNode temp = head;
+//         int index = lengthHead - n;
+//         int count = 0;
+//         if(head == null) return head;
+//         //remove at head
+//         if(index == 0)
+//         {
+//             return head.next;
+//         }
+//         while(temp != null)
+//         {
+//             if(count + 1 == index)
+//             {
+                 
+//                     temp.next = temp.next.next;
+//                     break;
+//             }
+//             count++;
+//             temp = temp.next;
+//         }
+//         return head;
+
+//     }
+//     public static int length(ListNode head)
+//     {
+//         int count = 0;
+//         ListNode temp = head;
+//         while(temp != null)
+//         {
+//             count++;
+//             temp = temp.next;
+//         }
+//         return count;
+//     }
+// }
