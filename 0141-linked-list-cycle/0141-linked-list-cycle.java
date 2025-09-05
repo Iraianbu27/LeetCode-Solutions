97585715a0b2767bc15b3f4508587525f3c6ed26
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-05 09:15:43 +0530

    Time: 0 ms (100%), Space: 44.8 MB (27.72%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,24 +11,38 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        Map<ListNode,Integer> mpp = new HashMap<>();
-        if(head == null)
+      ListNode temp = head;
+      ListNode slow = head;
+      ListNode fast = head;
+      while(fast != null && fast.next != null)
+      {
+        slow = slow.next;
+        fast = fast.next.next;
+        if(fast == slow)
         {
-            return false;
+            return true;
         }
-        ListNode temp = head;
-        while(temp != null)
-        {
-            if(!mpp.containsKey(temp)){
-                 
-                mpp.put(temp,1);
-            }
-            else{
-                return true;
-            }
-            temp = temp.next;
-
-        }
-        return false;
+      }
+      return false;
     }
 }
+//BRUTE FORCE USING HASH MAP
+  // Map<ListNode,Integer> mpp = new HashMap<>();
+        // if(head == null)
+        // {
+        //     return false;
+        // }
+        // ListNode temp = head;
+        // while(temp != null)
+        // {
+        //     if(!mpp.containsKey(temp)){
+                 
+        //         mpp.put(temp,1);
+        //     }
+        //     else{
+        //         return true;
+        //     }
+        //     temp = temp.next;
+
+        // }
+        // return false;
