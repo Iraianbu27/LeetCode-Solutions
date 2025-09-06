1fc72b56762acaaaaf62fd205d8c4c6a45958b97
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-06 09:14:46 +0530

    Time: 11 ms (39.05%), Space: 56.5 MB (71.07%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -11,21 +11,92 @@
  import java.util.*;
 class Solution {
     public ListNode sortList(ListNode head) {
-        List<Integer> list = new ArrayList<>();
         ListNode temp = head;
-        ListNode temp1 = head;
-        while(temp != null)
+       return merge( temp);
+    }
+    public static ListNode merge(ListNode head)
+    {
+        if(head == null || head.next == null)
         {
-            list.add(temp.val);
-            temp = temp.next;
+            return head;
         }
-        Collections.sort(list);
-        int i = 0;
-        while(temp1 != null)
+        //mid
+        ListNode mid = middle(head);
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null;
+        left = merge(left);
+        right = merge(right);
+        return mergeSort(left,right);
+
+    }
+    //finding the mid
+    public static ListNode middle(ListNode head)
+    {
+        ListNode temp = head;
+        ListNode fast = temp;
+        ListNode slow = temp;
+        while(  fast.next != null && fast.next.next != null)
         {
-            temp1.val = list.get(i++);
-            temp1 = temp1.next;
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return head;
+        return slow;
+    }
+    //merging
+    public static ListNode mergeSort(ListNode left,ListNode right)
+    {
+        ListNode newHead = new ListNode(-1);
+        ListNode temp = newHead;
+        while(left != null && right != null)
+        {
+            if(left.val <= right.val)
+            {
+                temp.next = left;
+                temp = left;
+                left = left.next;
+            }
+            else{
+                temp.next = right;
+                temp = right;
+                right = right.next;
+            }
+        }
+        while( left != null)
+        {
+            temp.next = left;
+            temp = left;
+            left = left.next;
+        }
+        while( right != null)
+        {
+            temp.next = right;
+            temp = right;
+            right = right.next;
+        }
+        return newHead.next;
+
     }
 }
+//BRUTE FORCE USING EXTRA SPACE
+// import java.util.*;
+// class Solution {
+//     public ListNode sortList(ListNode head) {
+//         List<Integer> list = new ArrayList<>();
+//         ListNode temp = head;
+//         ListNode temp1 = head;
+//         while(temp != null)
+//         {
+//             list.add(temp.val);
+//             temp = temp.next;
+//         }
+//         Collections.sort(list);
+//         int i = 0;
+//         while(temp1 != null)
+//         {
+//             temp1.val = list.get(i++);
+//             temp1 = temp1.next;
+//         }
+//         return head;
+//     }
+// }
