f66d6bbe46e79553bd453c96c6e3029ca662fa1d
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-12 08:07:04 +0530

    Time: 4 ms (34.87%), Space: 44.3 MB (40.99%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,20 +1,9 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        //1.fill the nums1 arr with index of matching ele from nums2\
-        for(int i = 0;i<nums1.length;i++)
-        {
-            for(int j = 0;j<nums2.length;j++)
-            {
-                if(nums2[j] == nums1[i])
-                {
-                    nums1[i] = j;
-                    break;
-                }
-            }
-        }
         
-        //2.reverse traverse to find the greatest and store in new arr
-        int[] nge = new int[nums2.length];
+        
+        //traverse nums2 store the nge in mpp with that ele
+        Map<Integer,Integer> mpp = new HashMap<>();
         Stack<Integer> st = new Stack<>();
         int n = nums2.length;
         for(int i = n-1;i>=0;i--)
@@ -25,21 +14,66 @@ class Solution {
             }
             if(st.isEmpty())
             {
-                nge[i] = -1;
+                mpp.put(nums2[i],-1);
                 st.push(nums2[i]);
             }
             else{
-                nge[i] = st.peek();
+                // nge[i] = st.peek();
+                mpp.put(nums2[i],st.peek());
                 st.push(nums2[i]);
             }
 
         }
-        //3.update the nums2 array's greatest ele in nums1 for stored index
+        //2.using mpp update nge of nums1
         for(int i = 0;i<nums1.length;i++)
         {
-            nums1[i] = nge[nums1[i]];
+            if(mpp.containsKey(nums1[i]))
+            {
+                nums1[i] = mpp.get(nums1[i]);
+            }
         }
         return nums1;
+         
 
     }
 }
+  // //1.fill the nums1 arr with index of matching ele from nums2\
+        // for(int i = 0;i<nums1.length;i++)
+        // {
+        //     for(int j = 0;j<nums2.length;j++)
+        //     {
+        //         if(nums2[j] == nums1[i])
+        //         {
+        //             nums1[i] = j;
+        //             break;
+        //         }
+        //     }
+        // }
+        
+        // //2.reverse traverse to find the greatest and store in new arr
+        // int[] nge = new int[nums2.length];
+        // Stack<Integer> st = new Stack<>();
+        // int n = nums2.length;
+        // for(int i = n-1;i>=0;i--)
+        // {
+        //     while(!st.isEmpty() && nums2[i] >= st.peek())
+        //     {
+        //         st.pop();
+        //     }
+        //     if(st.isEmpty())
+        //     {
+        //         nge[i] = -1;
+        //         st.push(nums2[i]);
+        //     }
+        //     else{
+        //         nge[i] = st.peek();
+        //         st.push(nums2[i]);
+        //     }
+
+        // }
+        // //3.update the nums2 array's greatest ele in nums1 for stored index
+        // for(int i = 0;i<nums1.length;i++)
+        // {
+        //     nums1[i] = nge[nums1[i]];
+        // }
+        // return nums1;
