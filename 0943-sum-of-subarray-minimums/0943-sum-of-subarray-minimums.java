e0ab12f5fe3f699c49e1d185da8d493d1dd5b794
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-15 09:13:42 +0530

    Time: 73 ms (32.38%), Space: 51 MB (63.62%) - LeetHub

---
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
@@ -0,0 +1,65 @@
+class Solution {
+    public static int[] nsee(int[] arr)
+    {
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[n];
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.isEmpty() && arr[i] < arr[st.peek()])
+            {
+                st.pop();
+            }
+            if(!st.isEmpty())
+            {
+                ans[i] = st.peek();
+            }
+            else{
+                ans[i] = n;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    public static int[] pse(int[] arr)
+    {
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            while(!st.isEmpty() && arr[i] <= arr[st.peek()])
+            {
+                st.pop();
+            }
+            if(!st.isEmpty())
+            {
+                ans[i] = st.peek();
+                 
+            }
+            else{
+                ans[i] = -1;
+                
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    public int sumSubarrayMins(int[] arr) {
+        int[] nextArr = nsee(arr);
+        int[] prevArr = pse(arr);
+        long total = 0;
+        long finalOp = 0;
+        int mod = (int)1e9+7;
+        for(int i = 0;i<arr.length;i++)
+        {
+            int left = i - prevArr[i];
+            int right = nextArr[i] - i;
+            total = ( (long)left*right*arr[i])%mod;
+            finalOp  = (finalOp + total)%mod;
+
+
+        }
+        return (int)finalOp;
+    }
+}
