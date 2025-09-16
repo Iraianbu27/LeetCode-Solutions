8c369082580ae7fd43ea6a4ee692b024e5626882
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-16 12:01:33 +0530

    Time: 14 ms (85.21%), Space: 46.3 MB (54.99%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -0,0 +1,54 @@
+class Solution {
+    public static int LargestRectHistogram(int[] arr){
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+        int total = 0;
+        for(int i = 0;i<n;i++)
+        {
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i])
+            {
+                int ele = st.pop();
+                int area= (arr[ele] * (i  - (st.isEmpty() ? -1 : st.peek()) - 1 ));
+                total = Math.max(area,total);
+            }
+            st.push(i);
+        }
+        while(!st.isEmpty())
+        {
+            int ele = st.pop();
+            int area= (arr[ele] * (n  - (st.isEmpty() ? -1 : st.peek()) - 1 ));
+            total = Math.max(area,total);
+        }
+        return total;
+    }
+
+    public int maximalRectangle(char[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] prefixSum = new int[matrix.length][matrix[0].length];
+        int col = 0;
+        while(col<m)
+        {
+            int prSum = 0;
+            for(int i = 0;i<n;i++)
+            {
+                int val =  matrix[i][col] - '0';
+                if(val == 0)
+                {
+                    prSum = 0;
+                }
+                else{
+                    prSum += val;
+                }
+                prefixSum[i][col] = prSum;
+            }
+            col++;
+        }
+        int area = 0;
+        for(int i = 0;i<n;i++)
+        {
+              area = Math.max(area,LargestRectHistogram(prefixSum[i]));
+        }
+        return area;
+    }
+}
