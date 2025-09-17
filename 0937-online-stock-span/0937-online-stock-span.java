5ce52bafd8108c293a0f18ef47a9262e5b3876d5
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-17 12:19:03 +0530

    Time: 35 ms (19.29%), Space: 54.9 MB (84.86%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.java
+++ 0937-online-stock-span/0937-online-stock-span.java
@@ -1,32 +1,50 @@
 class StockSpanner {
-    List<Integer> list = new ArrayList<>();
-    // public StockSpanner() {
-        
-    // }
+    List<Integer> list;
+    Stack<Integer> st;
+    int index = -1;
+    public StockSpanner() {
+        list = new ArrayList<>();
+        st = new Stack<>();
+        index = -1;
+    }
     
     public int next(int price) {
-        list.add(price);
+        index = index + 1;
         int span = 0;
-        if(list.size() == 1)
-        {
-            return 1;
-        }
-        else{
-            int currDay = list.get(list.size() - 1);
-            for(int i = list.size()-1;i>=0;i--)
+            while(!st.isEmpty() && price >= list.get(st.peek()) )
             {
-                if(list.get(i) <= currDay)
-                {
-                    span++;
-                }
-                else{
-                    break;
-                }
+                // list.get(st.peek());
+                st.pop();
+                
             }
-        }
+            span = index - (st.isEmpty() ? -1 : st.peek());
+            st.push(index);
+            list.add(price);
+
+        
         return span;
+       
+       
     }
 }
+ // if(list.size() == 1)
+        // {
+        //     return 1;
+        // }
+        // else{
+        //     int currDay = list.get(list.size() - 1);
+        //     for(int i = list.size()-1;i>=0;i--)
+        //     {
+        //         if(list.get(i) <= currDay)
+        //         {
+        //             span++;
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        // }
+        // return span;
 
 /**
  * Your StockSpanner object will be instantiated and called as such:
