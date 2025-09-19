1759ec6f02605a964a42c81407581f4478d2e8d5
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-19 11:06:56 +0530

    Time: 1 ms (99.48%), Space: 57.4 MB (20.79%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,7 +4,8 @@ class Solution {
         for(int i = 0;i<nums.length;i++)
         {
             sum += nums[i];
-            max = Math.max(max,sum);
+            // max = Math.max(max,sum);
+            max = (sum > max) ? sum : max;
             if(sum < 0) sum = 0;
         }
         return max;      
