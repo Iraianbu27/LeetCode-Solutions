cf7c873e17dcf7ab75e0c69c27d6da54294dfdee
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-20 08:18:42 +0530

    Time: 0 ms (100%), Space: 42 MB (90.67%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,32 +1,71 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {       
-        int i = 0,j=0,index = 0;
-        int[] arr = new int[m+n];
-        while(i< m && j < n)
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(i >= 0 && j >= 0)
         {
-            if(nums1[i] <= nums2[j])
+            if(nums1[i] > nums2[j])
             {
-                arr[index++] = nums1[i++];
+                nums1[k--] = nums1[i--];
+              
             }
             else{
-                arr[index++] = nums2[j++];
+                nums1[k--] = nums2[j--];
+              
+              
             }
         }
-        while(i<m)
+        while(j>=0)
         {
-            arr[index++] = nums1[i++];
+            nums1[k--] = nums2[j--];
         }
-        while(j<n)
-        {
-            arr[index++] = nums2[j++];
-        }
-        for(int o = 0;o<m+n;o++)
-        {
-            nums1[o]=arr[o];
-        }
-
     }
 }
+    
+    //  int i = m - 1;
+    //     int j = n - 1;
+    //     int k = m + n - 1;
+    //     while(i >= 0 && j >= 0) {
+    //         if(nums1[i] > nums2[j]) {
+    //             nums1[k--] = nums1[i--];
+    //         }
+    //         else {
+    //             nums1[k--] = nums2[j--];
+    //         }
+
+    //     }
+    //     while(j >= 0) {
+
+    //         nums1[k--] = nums2[j--];
+    //     }
+    // }
+    //     int i = 0,j=0,index = 0;
+    //     int[] arr = new int[m+n];
+    //     while(i< m && j < n)
+    //     {
+    //         if(nums1[i] <= nums2[j])
+    //         {
+    //             arr[index++] = nums1[i++];
+    //         }
+    //         else{
+    //             arr[index++] = nums2[j++];
+    //         }
+    //     }
+    //     while(i<m)
+    //     {
+    //         arr[index++] = nums1[i++];
+    //     }
+    //     while(j<n)
+    //     {
+    //         arr[index++] = nums2[j++];
+    //     }
+    //     for(int o = 0;o<m+n;o++)
+    //     {
+    //         nums1[o]=arr[o];
+    //     }
+
+ //---------------------
 // brute force (O(n log n + 3)) ~ O(n log n)
 // for(int i = m;i<nums1.length;i++)
 //         {
