449da05635d6f1659bc8af0e7bed83ce3cb4449e
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-25 08:25:38 +0530

    Time: 10 ms (79.11%), Space: 49 MB (16.96%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int gLen = g.length;
+        int sLen = s.length;
+        int l=0,r=0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        while(l<gLen && r<sLen)
+        {
+            if(g[l] <= s[r]){
+                l++;
+                r++;
+            }
+            else{
+                r++;
+            }
+        }
+        return l;
+    }
+}
+//int gLen = g.length;
+        // int sLen = s.length;
+        // int count = 0;
+        // boolean[] used = new boolean[sLen];
+        // for(int i = 0;i<gLen;i++)
+        // {
+        //     for(int j = 0;j<sLen;j++)
+        //     {
+        //         if(!used[j] && s[j] >= g[i])
+        //         {
+        //             used[j] = true;
+        //             count++;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return count;
