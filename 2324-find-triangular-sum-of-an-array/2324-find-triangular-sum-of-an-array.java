80160a9d40a833178ed9825043b4574df9ac54dd
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-09-30 17:27:07 +0530

    Time: 42 ms (86.32%), Space: 44.1 MB (98.11%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.java
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.java
@@ -1,19 +1,29 @@
 class Solution {
-    public int recFunc(int[] nums,int n){
-        
-        if(n == 1){
-        return nums[0];
-        }
-        int[] newNums = new int[n-1];
-        for(int i = 0;i<newNums.length;i++){
-            newNums[i] = (nums[i] + nums[i+1]) % 10;
-        }
-        return recFunc(newNums,newNums.length);
-
-    }
-
     public int triangularSum(int[] nums) {
-        return recFunc(nums ,nums.length);
+        int n = nums.length;
+        for(int size = n;size>1;size--){
+            for(int i = 0;i<size - 1;i++){ // we comppar i and i + 1 so size - 1
+                nums[i] = (nums[i] + nums[i+1]) % 10;
+            } 
+        }
+        return nums[0];
     }
 
 }
+// recursive approach
+// public int recFunc(int[] nums,int n){
+        
+    //     if(n == 1){
+    //     return nums[0];
+    //     }
+    //     int[] newNums = new int[n-1];
+    //     for(int i = 0;i<newNums.length;i++){
+    //         newNums[i] = (nums[i] + nums[i+1]) % 10;
+    //     }
+    //     return recFunc(newNums,newNums.length);
+
+    // }
+
+    // public int triangularSum(int[] nums) {
+    //     return recFunc(nums ,nums.length);
+    // }
