3a1f8485e20e47dd84a3965340419fbadbc120a0
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-01 08:37:41 +0530

    Time: 1 ms (98.83%), Space: 44.9 MB (61.17%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,41 +1,76 @@
 import java.util.*;
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> list=new ArrayList<>();
-        List<int[]> interval = new ArrayList<>();
-        for(int[] i : intervals){
-            list.add(i);
+        int n = intervals.length;
+        int i = 0;
+        List<int[]> list = new ArrayList<>();
+
+
+        //adding intervals till new interval to list
+        while(i<n && intervals[i][1] < newInterval[0]){
+            list.add(intervals[i]);
+            i++;
+        }
+
+        //adding overlapping interval with newInterval to list
+        while(i<n && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = Math.min(intervals[i][0],newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1],newInterval[1]);
+            i++;
         }
         list.add(newInterval);
 
-        //sorting
-        Collections.sort(list,(a,b)->{
-            return Integer.compare(a[0],b[0]);
-        });
-
-        //merging overlapping intervals
-        
-        for(int i = 0;i<list.size();i++){
-            // if(interval.length != 0){
-            //     int end = interval.get(interval.size() - 1)[1];
-            // }
-            // int start = list.get(i).[0];
-            if(interval.size() == 0 || list.get(i)[0] > interval.get(interval.size() - 1)[1]){
-                interval.add(list.get(i));
-            }
-            else{
-                int end = Math.max(interval.get(interval.size() - 1)[1],list.get(i)[1]);
-                interval.get(interval.size() -1)[1] = end;
-            }
+        //adding remaing interval to list
+        while(i<n){
+            list.add(intervals[i]);
+            i++;
         }
-
-        int[][] arr = new int[interval.size()][interval.get(0)[0]];
+        
+        int[][] arr = new int[list.size()][list.get(0)[0]];
         int index = 0;
-        for(int[] i : interval){
-            arr[index] = i;
+        for(int[] j : list){
+            arr[index] = j;
             index++;
         }
         return arr;
-        
+
     }
 }
+//brute force 
+    //     List<int[]> list=new ArrayList<>();
+    //     List<int[]> interval = new ArrayList<>();
+    //     for(int[] i : intervals){
+    //         list.add(i);
+    //     }
+    //     list.add(newInterval);
+
+    //     //sorting
+    //     Collections.sort(list,(a,b)->{
+    //         return Integer.compare(a[0],b[0]);
+    //     });
+
+    //     //merging overlapping intervals
+        
+    //     for(int i = 0;i<list.size();i++){
+    //         // if(interval.length != 0){
+    //         //     int end = interval.get(interval.size() - 1)[1];
+    //         // }
+    //         // int start = list.get(i).[0];
+    //         if(interval.size() == 0 || list.get(i)[0] > interval.get(interval.size() - 1)[1]){
+    //             interval.add(list.get(i));
+    //         }
+    //         else{
+    //             int end = Math.max(interval.get(interval.size() - 1)[1],list.get(i)[1]);
+    //             interval.get(interval.size() -1)[1] = end;
+    //         }
+    //     }
+
+    //     int[][] arr = new int[interval.size()][interval.get(0)[0]];
+    //     int index = 0;
+    //     for(int[] i : interval){
+    //         arr[index] = i;
+    //         index++;
+    //     }
+    //     return arr;
+        
+    // }
