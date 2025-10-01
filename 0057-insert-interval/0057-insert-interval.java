872a23281643a7c6349ed37d579e510ef4636b4f
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-01 08:59:03 +0530

    Time: 1 ms (98.83%), Space: 45 MB (61.17%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -13,6 +13,7 @@ class Solution {
         }
 
         //adding overlapping interval with newInterval to list
+        //we compare
         while(i<n && intervals[i][0] <= newInterval[1]){
             newInterval[0] = Math.min(intervals[i][0],newInterval[0]);
             newInterval[1] = Math.max(intervals[i][1],newInterval[1]);
