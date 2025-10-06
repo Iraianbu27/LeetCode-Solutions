6d051861a5b4538d156f110b5299dc6ca879129f
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-06 10:51:02 +0530

    Time: 140 ms (58.64%), Space: 41.9 MB (36.96%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -17,28 +17,31 @@ public class Solution {
         
         return false;
     }
-    
-    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
-        if (index == word.length()) {
+    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j , int index)
+    {
+        if(word.length() == index)
+        {
             return true;
         }
-        
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
+        if(i<0 || i>=board.length || j<0 || j>= board[0].length || board[i][j] != word.charAt(index) || visited[i][j] == true){
             return false;
         }
         
         visited[i][j] = true;
-        
-        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
-            backtrack(board, word, visited, i - 1, j, index + 1) ||
-            backtrack(board, word, visited, i, j + 1, index + 1) ||
-            backtrack(board, word, visited, i, j - 1, index + 1)) {
+        if(
+        backtrack(board,word,visited,i+1,j,index + 1) ||
+        backtrack(board,word,visited,i,j-1,index + 1) ||
+        backtrack(board,word,visited,i-1,j,index + 1) ||
+        backtrack(board,word,visited,i,j+1,index + 1) ){
+            visited[i][j] = false;
             return true;
         }
+    visited[i][j] = false;
+    return false;
         
-        visited[i][j] = false;
-        return false;
     }
+    
+   
 }
 
 
