dc40222eb7570009b3a96f844d0bf3baa807d36b
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-06 16:31:03 +0530

    Time: 1 ms (99.81%), Space: 44.9 MB (57.72%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,39 +1,39 @@
 class Solution {
-    public boolean valid(char[][] board,int row,int col,int n){
-        int r = row;
-        int c = col;
-        //left
-        while(c>=0){
-            if(board[r][c] =='Q'){
-                return false;
-            }
-            c--;
-        }
+    // public boolean valid(char[][] board,int row,int col,int n){
+    //     int r = row;
+    //     int c = col;
+    //     //left
+    //     while(c>=0){
+    //         if(board[r][c] =='Q'){
+    //             return false;
+    //         }
+    //         c--;
+    //     }
 
-        //diagonal upward
-        r = row;
-        c = col;
-        while(r>=0 && c>= 0){
-            if(board[r][c] == 'Q'){
-                return false;
-            }
-            r--;
-            c--;
-        }
+    //     //diagonal upward
+    //     r = row;
+    //     c = col;
+    //     while(r>=0 && c>= 0){
+    //         if(board[r][c] == 'Q'){
+    //             return false;
+    //         }
+    //         r--;
+    //         c--;
+    //     }
 
-        //diagonal downward
-        r=row;
-        c=col;
-        while(c >=0 && r<n){
-            if(board[r][c] =='Q'){
-                return false;
-            }
-            r++;
-            c--;
-        }
+    //     //diagonal downward
+    //     r=row;
+    //     c=col;
+    //     while(c >=0 && r<n){
+    //         if(board[r][c] =='Q'){
+    //             return false;
+    //         }
+    //         r++;
+    //         c--;
+    //     }
 
-        return true;
-    }
+    //     return true;
+    // }
 
     public List<String> convert(char[][] board,int n){
         List<String> list = new ArrayList<>();
@@ -45,29 +45,38 @@ class Solution {
     }
 
 
-    public void backtracking(char[][] board,List<List<String>> list,int row,int col,int n){
+    public void backtracking(char[][] board,List<List<String>> list,int col,int n,int[] left,int[] lowDia,int[] upDia){
         //basecase
         if(col == n){
             list.add(convert(board,n));
             return;
         } 
         for(int r = 0;r<n;r++){
-            if(valid(board,r,col,n)){
+            if(left[r] == 0 && upDia[r+col] == 0 && lowDia[n-1 + col-r] == 0){
+                left[r] = 1;
+                upDia[r+col] = 1;
+                lowDia[n-1 + col-r] = 1;
                 board[r][col] ='Q';
-                backtracking(board,list,r,col+1,n);
+                backtracking(board,list,col+1,n,left,lowDia,upDia);
                 board[r][col] = '.';
+                left[r] = 0;
+                upDia[r+col] = 0;
+                lowDia[n-1 + col-r] = 0;
             }
         }
     }
     public List<List<String>> solveNQueens(int n) {
         char[][] board = new char[n][n];
+        int[] leftBoard = new int[n];
+        int[] lowerDiagonal = new int[2*n-1];
+        int[] upperDiagonal = new int[2*n-1];
         for(int i = 0;i<n;i++){
             for(int j = 0;j<n;j++){
                 board[i][j] = '.';
             }
         }
         List<List<String>> list = new ArrayList<>();
-        backtracking(board,list,0,0,n);
+        backtracking(board,list,0,n,leftBoard,lowerDiagonal,upperDiagonal);
         return list;
     }
 }
