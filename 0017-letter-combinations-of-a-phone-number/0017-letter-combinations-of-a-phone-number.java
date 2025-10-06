f0ad0c012f4ea2ee6857fe32b8517ef37e7ad751
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-06 17:58:00 +0530

    Time: 0 ms (100%), Space: 42.1 MB (78.76%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,63 @@
+// class Solution {
+//     public void backtracking(char[] input,Map<Character,String[]> mpp,List<List<String>> list,int n,int r, int c,List<String> ds,String s){
+//         if(r == n-1 || c == n-1){
+//             return;
+//         }
+        
+//     }
+//     public List<String> letterCombinations(String digits) {
+//         List<List<String>> list = new ArrayList<>();
+//         Map<Character,  String[]> mpp = new HashMap<>();
+//         mpp.put('2', new String[]{"a", "b", "c"});
+//         mpp.put('3', new String[]{"d", "e", "f"});
+//         mpp.put('4', new String[]{"g", "h", "i"});
+//         mpp.put('5', new String[]{"j", "k", "l"});
+//         mpp.put('6', new String[]{"m", "n", "o"});
+//         mpp.put('7', new String[]{"p", "q", "r", "s"});
+//         mpp.put('8', new String[]{"t", "u", "v"});
+//         mpp.put('9', new String[]{"w", "x", "y", "z"});
+//         List<String> ds = new ArrayList<>();
+      
+//         char[] input = digits.toCharArray();
+//         backtracking(input,mpp,list,input.length,0,0,"");
+
+         
+//     }
+// }
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if(digits.isEmpty()) return new ArrayList<>();
+        
+        // Map digit to letters
+        Map<Character, String[]> mpp = new HashMap<>();
+        mpp.put('2', new String[]{"a", "b", "c"});
+        mpp.put('3', new String[]{"d", "e", "f"});
+        mpp.put('4', new String[]{"g", "h", "i"});
+        mpp.put('5', new String[]{"j", "k", "l"});
+        mpp.put('6', new String[]{"m", "n", "o"});
+        mpp.put('7', new String[]{"p", "q", "r", "s"});
+        mpp.put('8', new String[]{"t", "u", "v"});
+        mpp.put('9', new String[]{"w", "x", "y", "z"});
+        
+        List<String> result = new ArrayList<>();
+        backtrack(digits, 0, new StringBuilder(), result, mpp);
+        return result;
+    }
+    
+    private void backtrack(String digits, int index, StringBuilder current, 
+                          List<String> result, Map<Character, String[]> mpp) {
+        if(index == digits.length()) {
+            result.add(current.toString());
+            return;
+        }
+        
+        char digit = digits.charAt(index);
+        String[] letters = mpp.get(digit);
+        
+        for(String letter : letters) {
+            current.append(letter);
+            backtrack(digits, index + 1, current, result, mpp);
+            current.deleteCharAt(current.length() - 1);  // Backtrack
+        }
+    }
+}
