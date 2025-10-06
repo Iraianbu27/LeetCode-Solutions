fe716facef1486d3d9e00f1431debe493dd5183a
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-06 10:55:46 +0530

    Time: 135 ms (68.46%), Space: 41.3 MB (95.85%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,44 +2,41 @@ public class Solution {
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
-
-        boolean[][] visited = new boolean[m][n];
-        boolean result = false;
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    result = backtrack(board, word, visited, i, j, 0);
-                    if (result) return true;
+                if (backtrack(board, word, i, j, 0)) {
+                    return true;
                 }
             }
         }
-        
         return false;
     }
-    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j , int index)
-    {
-        if(word.length() == index)
-        {
+    
+    private boolean backtrack(char[][] board, String word, int i, int j, int index) {
+        if(index == word.length()) {
             return true;
         }
-        if(i<0 || i>=board.length || j<0 || j>= board[0].length || board[i][j] != word.charAt(index) || visited[i][j] == true){
+        
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || 
+           board[i][j] != word.charAt(index)) {
             return false;
         }
         
-        visited[i][j] = true;
-        boolean found = 
-       
-        backtrack(board,word,visited,i+1,j,index + 1) ||
-        backtrack(board,word,visited,i,j-1,index + 1) ||
-        backtrack(board,word,visited,i-1,j,index + 1) ||
-        backtrack(board,word,visited,i,j+1,index + 1);
-    visited[i][j] = false;
-    return found;
+        // Mark as visited by modifying the board
+        char temp = board[i][j];
+        board[i][j] = '#';  // Use a character that won't match
         
+        boolean found = backtrack(board, word, i+1, j, index+1) ||
+                        backtrack(board, word, i-1, j, index+1) ||
+                        backtrack(board, word, i, j+1, index+1) ||
+                        backtrack(board, word, i, j-1, index+1);
+        
+        // Restore original character (backtrack)
+        board[i][j] = temp;
+        
+        return found;
     }
-    
-   
 }
 
 
