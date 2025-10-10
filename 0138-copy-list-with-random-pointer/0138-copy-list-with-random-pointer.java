ee90aa9196ade6ff1412dd5ab816f9e85da7ccdc
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-10 19:16:51 +0530

    Time: 0 ms (100%), Space: 45.1 MB (6.51%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -14,28 +14,73 @@ class Node {
 */
 
 class Solution {
-    public Node copyRandomList(Node head) {
-        if(head == null) return head;
-        Map<Node,Node> mpp = new HashMap<>();
+    public void createCopy(Node head){
         Node temp = head;
-        //pushing original and copy node to map
         while(temp != null){
             Node copy = new Node(temp.val);
-            mpp.put(temp,copy);
-            temp = temp.next;
+            copy.next = temp.next;
+            temp.next = copy;
+            temp = temp.next.next;
         }
-        temp = head;
-        //building next to copy 
+    }
+    public void random(Node head){
+        Node temp = head;
         while(temp != null){
-            
-            Node copy = mpp.get(temp);
-            copy.next = mpp.get(temp.next);
-            copy.random = mpp.get(temp.random);
-            
-            temp = temp.next;
+            if(temp.random != null){
+                Node copy = temp.next;
+                copy.random = temp.random.next;
+            }
+            else{
+                Node copy = temp.next;
+                copy.random = null;
+            }
+            temp = temp.next.next;
         }
-        temp = head;
-        Node copy = mpp.get(temp);
-        return copy;
+    }
+    public Node next(Node head){
+        Node temp = head;
+        Node dNode = new Node(-1);
+        Node res = dNode;
+        while(temp != null){
+           res.next = temp.next;
+           res = temp.next;
+           
+           temp.next = temp.next.next;
+           res.next = null;
+           temp = temp.next;           
+        }
+        return dNode.next;
+
+    }
+    public Node copyRandomList(Node head) {
+       createCopy(  head);
+       random(  head);
+       return next(  head);
+
     }
 }
+//approach with TC of O(2n) sc O(2N)
+// public Node copyRandomList(Node head) {
+    //     if(head == null) return head;
+    //     Map<Node,Node> mpp = new HashMap<>();  sc  - O(n)
+    //     Node temp = head;
+    //     //pushing original and copy node to map
+    //     while(temp != null){             O(n)
+    //         Node copy = new Node(temp.val);  sc - O(n)
+    //         mpp.put(temp,copy);
+    //         temp = temp.next;
+    //     }
+    //     temp = head;
+    //     //building next to copy 
+    //     while(temp != null){    O(n)
+            
+    //         Node copy = mpp.get(temp);
+    //         copy.next = mpp.get(temp.next);
+    //         copy.random = mpp.get(temp.random);
+            
+    //         temp = temp.next;
+    //     }
+    //     temp = head;
+    //     Node copy = mpp.get(temp);
+    //     return copy;
+    // }
