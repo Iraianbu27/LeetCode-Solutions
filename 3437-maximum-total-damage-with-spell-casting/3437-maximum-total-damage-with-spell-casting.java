80e7d8f75ae8a78217c643c3c2d7f67a45a726b5
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-11 22:52:02 +0530

    Time: 424 ms (22.07%), Space: 63.8 MB (59.46%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.java
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.java
@@ -1,33 +1,27 @@
 class Solution {
+
     public long maximumTotalDamage(int[] power) {
-        Map<Integer, Long> freq = new HashMap<>();
-        for (int p : power) freq.put(p, freq.getOrDefault(p, 0L) + 1);
-
-        List<Integer> keys = new ArrayList<>(freq.keySet());
-        Collections.sort(keys);
-
-        int n = keys.size();
-        long[] dp = new long[n];
-        dp[0] = freq.get(keys.get(0)) * keys.get(0);
-
-        for (int i = 1; i < n; i++) {
-            long take = freq.get(keys.get(i)) * keys.get(i);
-            int prev = binarySearch(keys, i - 1, keys.get(i) - 3);
-            if (prev >= 0) take += dp[prev];
-            dp[i] = Math.max(dp[i - 1], take);
+        TreeMap<Integer, Integer> count = new TreeMap<>();
+        for (int p : power) {
+            count.put(p, count.getOrDefault(p, 0) + 1);
         }
-
-        return dp[n - 1];
-    }
-
-    private int binarySearch(List<Integer> keys, int end, int value) {
-        int l = 0, r = end, ans = -1;
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (keys.get(mid) <= value) {
-                ans = mid;
-                l = mid + 1;
-            } else r = mid - 1;
+        List<int[]> vec = new ArrayList<>();
+        vec.add(new int[] { -1000000000, 0 });
+        for (Map.Entry<Integer, Integer> e : count.entrySet()) {
+            vec.add(new int[] { e.getKey(), e.getValue() });
+        }
+        int n = vec.size();
+        long[] f = new long[n];
+        long mx = 0;
+        long ans = 0;
+        int j = 1;
+        for (int i = 1; i < n; i++) {
+            while (j < i && vec.get(j)[0] < vec.get(i)[0] - 2) {
+                mx = Math.max(mx, f[j]);
+                j++;
+            }
+            f[i] = mx + 1L * vec.get(i)[0] * vec.get(i)[1];
+            ans = Math.max(ans, f[i]);
         }
         return ans;
     }
