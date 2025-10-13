63c2ccc75dc3cb182091df16b0dd3606fcf018b8
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-13 12:18:52 +0530

    Time: 4 ms (98.16%), Space: 44.9 MB (77.51%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,49 +1,49 @@
-class MinStack {
-    Stack<Long> st  ;
-    long min ;
-    public MinStack() {
-        st = new Stack<>();
-        min = Long.MAX_VALUE;
-    }
+// class MinStack {
+//     Stack<Long> st  ;
+//     long min ;
+//     public MinStack() {
+//         st = new Stack<>();
+//         min = Long.MAX_VALUE;
+//     }
     
-    public void push(int vall) {
-        long val = (long)vall;
-        if(st.isEmpty())
-        {
-            min= val;
-            st.push(val);
-        }
-        else if(val> min){
-            st.push(val);
-        }
-        else{
-            long cal = (long)2*val - (long)min;
-            st.push(cal);
-            min = val;
-        }        
-    }
+//     public void push(int vall) {
+//         long val = (long)vall;
+//         if(st.isEmpty())
+//         {
+//             min= val;
+//             st.push(val);
+//         }
+//         else if(val> min){
+//             st.push(val);
+//         }
+//         else{
+//             long cal = (long)2*val - (long)min;
+//             st.push(cal);
+//             min = val;
+//         }        
+//     }
     
-    public void pop() {
-        long x = st.peek();
-        st.pop();
-         if(min > x)
-         {
-             min = (long)2*min - (long)x;
-         }
-    }
+//     public void pop() {
+//         long x = st.peek();
+//         st.pop();
+//          if(min > x)
+//          {
+//              min = (long)2*min - (long)x;
+//          }
+//     }
     
-    public int top() {
-        if(st.peek() > min)
-        {
-            return st.peek().intValue();
-        }
-        return (int)min;
-    }
+//     public int top() {
+//         if(st.peek() > min)
+//         {
+//             return st.peek().intValue();
+//         }
+//         return (int)min;
+//     }
     
-    public int getMin() {
-        return (int)min;
-    }
-}
+//     public int getMin() {
+//         return (int)min;
+//     }
+// }
 
 /**
  * Your MinStack object will be instantiated and called as such:
@@ -53,3 +53,122 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+
+
+
+class MinStack{
+    Stack<Integer> og;
+    Stack<Integer> min;
+    // int minimum;
+    // int capacity;
+    public MinStack(){
+        og = new Stack<>();
+        min = new Stack<>();
+        // minimum = Integer.MAX_VALUE;
+        // capacity = 0;
+    }
+    public void push(int x){
+        og.push(x);
+        if(min.isEmpty() || x <= min.peek()){
+            min.push(x);
+        }
+    }
+    public int pop(){
+        int val = og.pop();
+        if(val == min.peek()){
+            min.pop();
+        }
+        return val;
+
+    }
+    public int top(){
+        return og.peek();
+    }
+    public int getMin(){
+        return min.peek();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
