f5efe2b2ac17ac8435a87900a4dc66465b37a149
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-14 10:38:38 +0530

    Time: 19 ms (21.67%), Space: 46.7 MB (15.94%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -47,39 +47,57 @@ public int[] nextGreaterElements(int[] nums){
     int n = nums.length;
     int[] nge = new int[nums.length];
     Stack<Integer> st = new Stack<>();
-    for(int i = 2*n -1 ;i>=0;i--){
-        if(st.isEmpty()){
-            if(i < n-1){
-                nge[i] = -1;
+    // for(int i = 2*n -1 ;i>=0;i--){
+    //     if(st.isEmpty()){
+    //         if(i < n-1){
+    //             nge[i] = -1;
 
-            }
-             st.push(nums[i%n]);
-        }
-        else if(nums[i%n] >= st.peek()){
-            while(!st.isEmpty() && nums[i%n] >= st.peek()){
-                st.pop();
-            }
-            if(st.isEmpty()){
-                if(i < n){
-                    nge[i] = -1;
-                }
-                // st.push(nums[i%n]);
-            }
-            else{
-                if(i<n){
-                    nge[i] = st.peek();
-                }
+    //         }
+    //          st.push(nums[i%n]);
+    //     }
+    //     else if(nums[i%n] >= st.peek()){
+    //         while(!st.isEmpty() && nums[i%n] >= st.peek()){
+    //             st.pop();
+    //         }
+    //         if(st.isEmpty()){
+    //             if(i < n){
+    //                 nge[i] = -1;
+    //             }
+    //             // st.push(nums[i%n]);
+    //         }
+    //         else{
+    //             if(i<n){
+    //                 nge[i] = st.peek();
+    //             }
                 
-            }
-            st.push(nums[i%n]);
+    //         }
+    //         st.push(nums[i%n]);
+    //     }
+    //     else{
+    //         if(i < n){
+    //             nge[i] = st.peek();
+    //         }
+    //         st.push(nums[i%n]);
+    //     }
+
+    // }
+    for(int i = 2*n -1;i>=0;i--){
+        while(!st.isEmpty() && st.peek()<=nums[i%n]){
+            st.pop();
         }
-        else{
-            if(i < n){
+        if(i < n){
+            if(st.isEmpty()){
+                nge[i] = -1;
+                // st.push(nums[i]);
+            }
+            else if(nums[i] < st.peek()){
                 nge[i] = st.peek();
             }
+            st.push(nums[i]);
+        }
+        else{
             st.push(nums[i%n]);
         }
-
     }
     return nge;
 }
