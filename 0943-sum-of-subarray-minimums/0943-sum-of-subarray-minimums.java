831c7c6f8c98968c46572e654724c22a7feec786
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-15 11:03:33 +0530

    Time: 71 ms (40.32%), Space: 51 MB (61.39%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
@@ -6,7 +6,7 @@ class Solution {
         int[] ans = new int[n];
         for(int i=n-1;i>=0;i--)
         {
-            while(!st.isEmpty() && arr[i] < arr[st.peek()])
+            while(!st.isEmpty() && arr[i] <= arr[st.peek()])
             {
                 st.pop();
             }
@@ -28,7 +28,7 @@ class Solution {
         int[] ans = new int[n];
         for(int i=0;i<n;i++)
         {
-            while(!st.isEmpty() && arr[i] <= arr[st.peek()])
+            while(!st.isEmpty() && arr[i] < arr[st.peek()])
             {
                 st.pop();
             }
