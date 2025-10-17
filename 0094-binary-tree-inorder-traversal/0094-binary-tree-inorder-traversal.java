b16ca8a59be9585e200c9a1073cae810e7e2b0a9
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-17 11:44:56 +0530

    Time: 0 ms (100%), Space: 41.7 MB (66.99%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,17 +13,40 @@
  *     }
  * }
  */
-class Solution {
-    public void recFunction(TreeNode root,List<Integer> list){
-        if(root == null) return;
-        recFunction(root.left,list);
-        list.add(root.val);
-        recFunction(root.right,list);
-
-    }
+class Solution { 
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        recFunction(root,list);
+        List<Integer> list = new ArrayList<>(); 
+        Stack<TreeNode> st = new Stack<>();
+        // st.add(root);
+        TreeNode node = root;
+        while(node != null || !st.isEmpty()){
+            //traversing to the left most node
+            while(node!=null){
+                st.push(node);
+                node = node.left;
+            }
+            node = st.pop();
+            list.add(node.val);
+
+            //check for right child
+            node = node.right;
+
+        }
         return list;
     }
 }
+//USING RECURSSION
+// class Solution {
+//     public void recFunction(TreeNode root,List<Integer> list){
+//         if(root == null) return;
+//         recFunction(root.left,list);
+//         list.add(root.val); // in this point root node comes after left and right node comes at last 
+//         recFunction(root.right,list);
+
+//     }
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> list = new ArrayList<>();
+//         recFunction(root,list);
+//         return list;
+//     }
+// }
