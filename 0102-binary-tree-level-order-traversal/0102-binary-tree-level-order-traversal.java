9943ee7cab56c3689dd525b31147fb836539cef5
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-17 16:46:54 +0530

    Time: 0 ms (100%), Space: 45.1 MB (56.44%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,24 +13,44 @@
  *     }
  * }
  */
-class Solution {
+ class Solution {
+    public void recFunction(TreeNode root,List<List<Integer>> list,int level){
+        if(root == null) return;
+        if(level == list.size()){
+            list.add(new ArrayList<>());
+        }
+        list.get(level).add(root.val);
+        recFunction(root.left,list,level+1);
+        recFunction(root.right,list,level+1);
+    }
+    
     public List<List<Integer>> levelOrder(TreeNode root) {
         
         List<List<Integer>> list = new ArrayList<>();
-        if(root == null) return list;
-        Deque<TreeNode> dq = new ArrayDeque<>();
-        dq.addLast(root);
-        while(!dq.isEmpty()){
-            List<Integer> nodes = new ArrayList<>();
-            int size = dq.size();
-            for(int i = 0;i<size;i++){
-                TreeNode node = dq.removeFirst();
-                if(node.left != null) dq.addLast(node.left);
-                if(node.right != null) dq.addLast(node.right);
-                nodes.add(node.val);
-            }
-            list.add(nodes);
-        }
+        recFunction(root,list,0);
         return list;
+  
     }
 }
+ //using deque approach
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+        
+//         List<List<Integer>> list = new ArrayList<>();
+//         if(root == null) return list;
+//         Deque<TreeNode> dq = new ArrayDeque<>();
+//         dq.addLast(root);
+//         while(!dq.isEmpty()){
+//             List<Integer> nodes = new ArrayList<>();
+//             int size = dq.size();
+//             for(int i = 0;i<size;i++){
+//                 TreeNode node = dq.removeFirst();
+//                 if(node.left != null) dq.addLast(node.left);
+//                 if(node.right != null) dq.addLast(node.right);
+//                 nodes.add(node.val);
+//             }
+//             list.add(nodes);
+//         }
+//         return list;
+//     }
+// }
