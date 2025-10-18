2f03111af060c7fe07c458539ab780c2f1dad7a3
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-18 15:11:34 +0530

    Time: 0 ms (100%), Space: 45.1 MB (22.8%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -13,25 +13,40 @@
  *     }
  * }
  */
-class Solution {
-    int maxPath = 0;
-    public int height(TreeNode root){
+class Solution{
+    public int height(TreeNode root,int[] maxPath){
         if(root == null) return 0;
-        int lh = height(root.left);
-        int rh = height(root.right);
-        return Math.max(lh,rh) +1;
-    }
-    public void longestPath(TreeNode root){
-        if(root == null) return;
-        int lh = height(root.left);
-        int rh = height(root.right);
-        maxPath = Math.max(lh+rh,maxPath);
-        longestPath(root.left);
-        longestPath(root.right);
-    }
-    public int diameterOfBinaryTree(TreeNode root) {
+        int lh = height(root.left,maxPath);
+        int rh = height(root.right,maxPath);
+        maxPath[0] = Math.max(maxPath[0],rh+lh);
+        return Math.max(lh,rh) + 1;
         
-        longestPath(root);
-        return maxPath;
+    }
+    public int diameterOfBinaryTree(TreeNode root){
+        int[] maxPath = new int[1];
+        height(root,maxPath);
+        return maxPath[0];
     }
 }
+// class Solution {
+//     int maxPath = 0;
+//     public int height(TreeNode root){
+//         if(root == null) return 0;
+//         int lh = height(root.left);
+//         int rh = height(root.right);
+//         return Math.max(lh,rh) +1;
+//     }
+//     public void longestPath(TreeNode root){
+//         if(root == null) return;
+//         int lh = height(root.left);
+//         int rh = height(root.right);
+//         maxPath = Math.max(lh+rh,maxPath);
+//         longestPath(root.left);
+//         longestPath(root.right);
+//     }
+//     public int diameterOfBinaryTree(TreeNode root) {
+        
+//         longestPath(root);
+//         return maxPath;
+//     }
+// }
