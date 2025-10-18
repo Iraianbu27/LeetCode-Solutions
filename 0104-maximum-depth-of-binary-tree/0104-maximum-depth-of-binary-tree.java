7e73fca227284cc9e19002357bf35eb96864943b
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-18 11:17:31 +0530

    Time: 0 ms (100%), Space: 42.7 MB (49.79%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -13,45 +13,45 @@
  *     }
  * }
  */
- class Pair{
-    TreeNode node;
-    int depth;
-    public Pair(TreeNode node,int depth){
-        this.node = node;
-        this.depth = depth;
-    }
- }
- class Solution { 
-    public int maxDepth(TreeNode root) {
-        Stack<Pair> st = new Stack<>();
-        if(root == null) return 0;
-        st.push(new Pair(root,1));
-        int maxDepth = 0;
-        while(!st.isEmpty()){
-            Pair pair = st.pop();
-            TreeNode curr = pair.node;
-            int depth = pair.depth;
-            maxDepth = Math.max(maxDepth,depth);
-            if(curr.left != null) st.push(new Pair(curr.left,depth+1));
-            if(curr.right != null) st.push(new Pair(curr.right,depth+1));
-        }
-        return maxDepth;
-        
-    }
-}
-// class Solution {
-//     public int recFunction(TreeNode root){
-//         if(root == null) 
-//         {
-//             return 0;
-//         }
-//         int left = recFunction(root.left);
-//         int right = recFunction(root.right);
-//         // return Math.max(recFunction(root.left),recFunction(root.right)) + 1;
-//         return Math.max(left,right) + 1;
+//  class Pair{
+//     TreeNode node;
+//     int depth;
+//     public Pair(TreeNode node,int depth){
+//         this.node = node;
+//         this.depth = depth;
 //     }
+//  }
+//  class Solution { 
 //     public int maxDepth(TreeNode root) {
-//         return recFunction(root);
+//         Stack<Pair> st = new Stack<>();
+//         if(root == null) return 0;
+//         st.push(new Pair(root,1));
+//         int maxDepth = 0;
+//         while(!st.isEmpty()){
+//             Pair pair = st.pop();
+//             TreeNode curr = pair.node;
+//             int depth = pair.depth;
+//             maxDepth = Math.max(maxDepth,depth);
+//             if(curr.left != null) st.push(new Pair(curr.left,depth+1));
+//             if(curr.right != null) st.push(new Pair(curr.right,depth+1));
+//         }
+//         return maxDepth;
         
 //     }
 // }
+class Solution {
+    public int recFunction(TreeNode root){
+        if(root == null) 
+        {
+            return 0;
+        }
+        int left = recFunction(root.left);
+        int right = recFunction(root.right);
+        // return Math.max(recFunction(root.left),recFunction(root.right)) + 1;
+        return Math.max(left,right) + 1;
+    }
+    public int maxDepth(TreeNode root) {
+        return recFunction(root);
+        
+    }
+}
