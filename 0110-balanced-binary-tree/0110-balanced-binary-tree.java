293493d755c50e2ec2dda5e5d68767529df39820
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-18 13:07:50 +0530

    Time: 0 ms (100%), Space: 44.8 MB (15.12%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -13,30 +13,44 @@
  *     }
  * }
  */
+
+
 class Solution {
-    // public int rightHeight(TreeNode root){
-    //     if(root == null) return 0;
-    //     int lh = rightHeight(root.left);
-    //     int rh = rightHeight(root.right);
-    //     return Math.max(lh,rh)+1;
-    // }
-    public int leftHeight(TreeNode root){
+    public int recFunction(TreeNode root){
         if(root == null) return 0;
-        int lh = leftHeight(root.left);
-        int rh = leftHeight(root.right);
-        return Math.max(lh,rh) + 1;
-    }
-    public boolean recFunction(TreeNode root){
-        if(root == null) return true;
-        int lh = leftHeight(root.left);
-        int rh = leftHeight(root.right);
-        if(Math.abs(rh-lh)> 1) return false;
-        boolean left = recFunction(root.left);
-        boolean right = recFunction(root.right);
-        if(left == false || right == false) return false;
-        return true;
+        int lh = recFunction(root.left);
+        int rh = recFunction(root.right);
+        if(lh == -1 || rh == -1) return -1;
+        if(Math.abs(rh-lh) > 1) return -1;
+        
+        return Math.max(lh,rh)+1;
     }
     public boolean isBalanced(TreeNode root) {
-        return recFunction(root);
+        int i =recFunction(root);
+        if(i<0) return false;
+        return true;
     }
 }
+// two pass solution
+// class Solution {
+   
+//     public int leftHeight(TreeNode root){
+//         if(root == null) return 0;
+//         int lh = leftHeight(root.left);
+//         int rh = leftHeight(root.right);
+//         return Math.max(lh,rh) + 1;
+//     }
+//     public boolean recFunction(TreeNode root){
+//         if(root == null) return true;
+//         int lh = leftHeight(root.left);
+//         int rh = leftHeight(root.right);
+//         if(Math.abs(rh-lh)> 1) return false;
+//         boolean left = recFunction(root.left);
+//         boolean right = recFunction(root.right);
+//         if(left == false || right == false) return false;
+//         return true;
+//     }
+//     public boolean isBalanced(TreeNode root) {
+//         return recFunction(root);
+//     }
+// }
