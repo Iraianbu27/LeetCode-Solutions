0d6e44a15e14fd296f8aed65884658cf412bfe22
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-20 22:52:59 +0530

    Time: 1 ms (74.53%), Space: 43.3 MB (8.35%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int count = 0;
+        for(String operation : operations){
+            if(operation.equals("X++")){
+                count += 1;
+            }
+            else if(operation.equals("++X")){
+                count += 1;
+            }
+            else if(operation.equals("X--")){
+                count -= 1;
+            }
+            else{
+                count -= 1;
+            }
+        }
+        return count;
+    }
+}
