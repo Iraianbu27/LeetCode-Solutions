fc2b0a34f470a00dc014d945f71e669b1af448cc
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-22 14:52:26 +0530

    Time: 6 ms (100%), Space: 44.6 MB (85.83%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -7,41 +7,65 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ class Solution {
+    public TreeNode dfs(TreeNode root,TreeNode p,TreeNode q){
+        if(root == null)return root;
 
- //iterate and find all the path and use backtracking to find it correctly
- //if the root searching for is required then dont go any
-class Solution {
-    public void node2Leaf(TreeNode root,TreeNode p,TreeNode q,List<TreeNode> list,List<List<TreeNode>> result){
-        if(root == null) return;
-        list.add(root);
         if(root == p || root == q){
-            result.add(new ArrayList<>(list));
+            return root;
         }
-        
-            node2Leaf(root.left,p,q,list,result);
-            node2Leaf(root.right,p,q,list,result);
+        TreeNode lR = dfs(root.left,p,q);
+        TreeNode rR = dfs(root.right,p,q);
+        if(lR == null && rR == null) return null;
+        else if(lR != null && rR != null) return root;
+        else{
+            //lR != null || rR != null
+            if(lR != null) return lR;
+            else return rR;
+        }
+         
 
-        list.remove(list.size() - 1);    
-     
     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null)return null;
-        List<TreeNode> list = new ArrayList<>();
-        List<List<TreeNode>> result = new ArrayList<>();
-        node2Leaf(root,p,q,list,result);
-        List<TreeNode> list1 = result.get(0);
-        List<TreeNode> list2 = result.get(1);
-        int len = Math.min(list1.size(),list2.size());
-        TreeNode lastNode = null;
-        for(int i = 0;i<len;i++){
-            if(list1.get(i) == list2.get(i)){
-                lastNode = list1.get(i);
-            }
-            else{
-            break;
-            }
-        }
-        return lastNode;
+        return dfs(root,p,q);
 
     }
 }
+
+ //iterate and find all the path and use backtracking to find it correctly
+ //if the root searching for is required then add the temp list to result List to have the track of the path
+// class Solution {
+//     public void node2Leaf(TreeNode root,TreeNode p,TreeNode q,List<TreeNode> list,List<List<TreeNode>> result){
+//         if(root == null) return;
+//         list.add(root);
+//         if(root == p || root == q){
+//             result.add(new ArrayList<>(list));
+//         }
+        
+//             node2Leaf(root.left,p,q,list,result);
+//             node2Leaf(root.right,p,q,list,result);
+
+//         list.remove(list.size() - 1);    
+     
+//     }
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if(root == null)return null;
+//         List<TreeNode> list = new ArrayList<>(); //O(H)
+//         List<List<TreeNode>> result = new ArrayList<>(); //O(2H)
+//         node2Leaf(root,p,q,list,result); //O(N)
+//         List<TreeNode> list1 = result.get(0); //O(H)
+//         List<TreeNode> list2 = result.get(1);//O(H)
+//         int len = Math.min(list1.size(),list2.size());
+//         TreeNode lastNode = null;
+//             for(int i = 0;i<len;i++){ //O(H)
+//                 if(list1.get(i) == list2.get(i)){
+//                     lastNode = list1.get(i);
+//                 }
+//             else{
+//                 break;
+//             }
+//         }
+//         return lastNode;
+
+//     }
+// }
