418b726bda52e9db3181a7792c9772aec0178bb9
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-23 14:05:57 +0530

    Time: 0 ms (100%), Space: 47.6 MB (78.83%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -13,16 +13,51 @@
  *     }
  * }
  */
-class Solution {
-    public void dfs(TreeNode root,int[] count){
-        if(root == null) return;
-        count[0]++;
-        dfs(root.left,count);
-        dfs(root.right,count);
+
+class Solution{
+    public int getLeftHeight(TreeNode root){
+        int count = 0;
+        while(root != null){
+            count++;
+            root=root.left;
+        }
+        return count;
+    }
+    public int getRightHeight(TreeNode root){
+        int count = 0;
+        while(root != null){
+            count++;
+            root = root.right;
+        }
+        return count;
+    }
+    public int dfs(TreeNode root){
+        if(root == null)return 0;
+        int lH = getLeftHeight(root);
+        int rH = getRightHeight(root);
+        if(lH == rH){
+            return (1<<lH)-1;
+        }
+        else{
+            return (dfs(root.left) + dfs(root.right) + 1);
+        }
     }
     public int countNodes(TreeNode root) {
-        int[] count = new int[1];
-        dfs(root,count);
-        return count[0];
+        return dfs(root);
     }
 }
+
+//O(N) appraoch
+// class Solution {
+//     public void dfs(TreeNode root,int[] count){
+//         if(root == null) return;
+//         count[0]++;
+//         dfs(root.left,count);
+//         dfs(root.right,count);
+//     }
+    // public int countNodes(TreeNode root) {
+    //     int[] count = new int[1];
+    //     dfs(root,count);
+    //     return count[0];
+    // }
+// }
