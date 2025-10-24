c4f597a3ae97997deed8e602e7fa68d52755188a
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-24 16:43:21 +0530

    Time: 0 ms (100%), Space: 41.9 MB (28.76%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,28 +13,60 @@
  *     }
  * }
  */
-class Solution { 
+ //morries algo
+ //1.case if left node is empty thn current node stands at root so print it and go through the new threads
+ //2.if the current node just passed throught the nwe thread then make the thread points to null coz its job is done
+ //3.which direction current moves after coming back from new thread go right(left is processed) , if the thread is just created then go right(left is not processed yet)
+ class Solution { 
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>(); 
-        Stack<TreeNode> st = new Stack<>();
-        // st.add(root);
-        TreeNode node = root;
-        while(node != null || !st.isEmpty()){
-            //traversing to the left most node
-            while(node!=null){
-                st.push(node);
-                node = node.left;
+        List<Integer> list = new ArrayList<>();
+        while(root != null){
+            if(root.left == null){
+                list.add(root.val);
+                root = root.right;
             }
-            node = st.pop();
-            list.add(node.val);
-
-            //check for right child
-            node = node.right;
-
+            else{
+                TreeNode temp = root.left;
+                while(temp.right != null && temp.right != root){
+                    temp = temp.right;
+                }
+                if(temp.right == null){
+                    temp.right = root;    //right thread builded to the root node from right most node on left sub tree
+                    root = root.left;
+                }
+                else{
+                    temp.right = null;
+                    list.add(root.val);
+                    root = root.right;   
+                }
+            }
         }
         return list;
     }
 }
+ //iterative method
+// class Solution { 
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> list = new ArrayList<>(); 
+//         Stack<TreeNode> st = new Stack<>();
+//         // st.add(root);
+//         TreeNode node = root;
+//         while(node != null || !st.isEmpty()){
+//             //traversing to the left most node
+//             while(node!=null){
+//                 st.push(node);
+//                 node = node.left;
+//             }
+//             node = st.pop();
+//             list.add(node.val);
+
+//             //check for right child
+//             node = node.right;
+
+//         }
+//         return list;
+//     }
+// }
 //USING RECURSSION
 // class Solution {
 //     public void recFunction(TreeNode root,List<Integer> list){
