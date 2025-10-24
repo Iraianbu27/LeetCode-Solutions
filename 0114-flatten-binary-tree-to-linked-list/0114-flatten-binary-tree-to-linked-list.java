93019c4041950b3e64a279f0b0396d61d5c91e8d
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-24 21:51:22 +0530

    Time: 0 ms (100%), Space: 42.2 MB (67.6%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -14,21 +14,45 @@
  * }
  */
  
-class Solution { 
+ //mories approach
+ class Solution{
     public void flatten(TreeNode root) { 
-        Stack<TreeNode> stack = new Stack<>();
-        if(root != null) stack.push(root);
-        // TreeNode prevNode = stack.peek();
-        while(!stack.isEmpty())
-        {
-            TreeNode temp = stack.pop();
-            if(temp.right != null) stack.push(temp.right);
-            if(temp.left != null) stack.push(temp.left);
-            if(!stack.isEmpty()) temp.right = stack.peek();
-            temp.left = null;
+        TreeNode currNode = root;
+        while(currNode != null){
+            if(currNode.left == null){
+                currNode = currNode.right;
+            }
+            else{
+                TreeNode temp = currNode.left;
+                while(temp.right != null){
+                    temp = temp.right;
+                }
+                temp.right = currNode.right;
+                currNode.right = currNode.left;
+                currNode.left = null;
+                currNode = currNode.right;
+                
+            }
         }
     }
-}
+ }
+
+ //stack method
+// class Solution { 
+    // public void flatten(TreeNode root) { 
+    //     Stack<TreeNode> stack = new Stack<>();
+    //     if(root != null) stack.push(root);
+    //     // TreeNode prevNode = stack.peek();
+    //     while(!stack.isEmpty())
+    //     {
+    //         TreeNode temp = stack.pop();
+    //         if(temp.right != null) stack.push(temp.right);
+    //         if(temp.left != null) stack.push(temp.left);
+    //         if(!stack.isEmpty()) temp.right = stack.peek();
+    //         temp.left = null;
+    //     }
+    // }
+// }
 
 
  //RECURSIVE SOLUTION
