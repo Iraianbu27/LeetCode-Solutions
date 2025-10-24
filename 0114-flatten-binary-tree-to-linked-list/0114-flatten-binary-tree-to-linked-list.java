d6270e93e49ec102452911760595cc0e33b1b32e
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-24 21:00:27 +0530

    Time: 1 ms (17.16%), Space: 42.1 MB (67.6%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -14,18 +14,37 @@
  * }
  */
  
-class Solution {
-    TreeNode prevNode = null;
-    public void dfs(TreeNode root){
-        if(root == null)return;
-        dfs(root.right);
-        dfs(root.left);
-        root.right = prevNode;
-        root.left = null;
-        prevNode = root;
-    }
+class Solution { 
     public void flatten(TreeNode root) { 
-        dfs(root);
-        // return root;
+        Stack<TreeNode> stack = new Stack<>();
+        if(root != null) stack.push(root);
+        // TreeNode prevNode = stack.peek();
+        while(!stack.isEmpty())
+        {
+            TreeNode temp = stack.pop();
+            if(temp.right != null) stack.push(temp.right);
+            if(temp.left != null) stack.push(temp.left);
+            if(!stack.isEmpty()) temp.right = stack.peek();
+            temp.left = null;
+        }
     }
 }
+
+
+ //RECURSIVE SOLUTION
+
+// class Solution {
+//     TreeNode prevNode = null;
+//     public void dfs(TreeNode root){
+//         if(root == null)return;
+//         dfs(root.right);
+//         dfs(root.left);
+//         root.right = prevNode;
+//         root.left = null;
+//         prevNode = root;
+//     }
+//     public void flatten(TreeNode root) { 
+//         dfs(root);
+//         // return root;
+//     }
+// }
