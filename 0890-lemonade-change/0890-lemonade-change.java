701ae302c0bca6ce17176aeeebd2bbea3f0a2e8e
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-29 12:08:01 +0530

    Time: 17 ms (5.16%), Space: 58.7 MB (5.06%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.java
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -1,37 +1,105 @@
+// class Solution {
+//     public boolean lemonadeChange(int[] bills) {
+//         int five = 0;
+//         int ten = 0;
+//         for(int i = 0;i<bills.length;i++)
+//         {
+//             if(bills[i] == 5)
+//             {
+//                 five++;
+//             }
+//             else if(bills[i] == 10 ){
+//                 if(five != 0){
+//                     ten++;
+//                     five--;
+//                 }
+//                 else{
+//                     return false;
+//                 }
+                
+//             }
+//             else if(bills[i] == 20){
+//                 if(five != 0 && ten != 0)
+//                 {
+//                     five--;
+//                     ten--;
+//                 }
+//                 else if(five >= 3){
+//                     five = five - 3;
+//                 }
+//                 else{
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int five = 0;
-        int ten = 0;
-        for(int i = 0;i<bills.length;i++)
-        {
-            if(bills[i] == 5)
-            {
-                five++;
+        Map<Integer,Integer> mpp = new HashMap<>();
+        int amt = 5;
+        mpp.put(5,0);
+        mpp.put(10,0);
+        mpp.put(20,0);
+
+        for(int j = 0;j<bills.length;j++){
+            if(bills[j] == 20){
+            int ten = mpp.get(10);
+            int five = mpp.get(5);
+                if(ten >=1 && five >= 1){
+                    mpp.put(20,mpp.get(20)+1);
+                    mpp.put(10,mpp.get(10)-1);
+                    mpp.put(5,mpp.get(5)-1);
+                }
+                else if(five >=3){
+                    mpp.put(5,mpp.get(5)-3);
+                    mpp.put(20,mpp.get(20)+1);                    
+                }
+                else {
+                    return false;
+                }
             }
-            else if(bills[i] == 10 ){
-                if(five != 0){
-                    ten++;
-                    five--;
+ 
+            else if(bills[j] == 10){
+                int five = mpp.get(5);
+                if(five >=1){
+                    mpp.put(5,mpp.get(5)-1);
+                    mpp.put(10,mpp.get(10)+1);
                 }
                 else{
                     return false;
                 }
-                
             }
-            else if(bills[i] == 20){
-                if(five != 0 && ten != 0)
-                {
-                    five--;
-                    ten--;
-                }
-                else if(five >= 3){
-                    five = five - 3;
-                }
-                else{
-                    return false;
-                }
+            else{
+                mpp.put(5,mpp.get(5)+1);
             }
         }
         return true;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
