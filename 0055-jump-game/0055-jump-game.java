df9b6a0cc0c9e96ab4bcaf6a9ee3496ae792a4ff
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-10-29 16:24:19 +0530

    Time: 2 ms (84.37%), Space: 45.7 MB (57.59%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,21 +1,21 @@
-class Solution {
-    public boolean canJump(int[] nums) {
-        int n = nums.length;
-        int maxJump = nums[0];
-        for(int i = 0;i<n;i++)
-        {
-            if(i > maxJump){
-                return false;  
-            }
+// class Solution {
+//     public boolean canJump(int[] nums) {
+//         int n = nums.length;
+//         int maxJump = nums[0];
+//         for(int i = 0;i<n;i++)
+//         {
+//             if(i > maxJump){
+//                 return false;  
+//             }
             
-            maxJump = Math.max(maxJump,i+nums[i]);
-            if(maxJump >= n-1) break;
+//             maxJump = Math.max(maxJump,i+nums[i]);
+//             if(maxJump >= n-1) break;
 
-        }
-        return true;
+//         }
+//         return true;
  
-    }
-}
+//     }
+// }
        // while(i < n){
         //     if(i == n-1){
         //         break;
@@ -28,3 +28,35 @@ class Solution {
         //     }
         // }
         // return i>=n-1;
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach= nums[0];
+        for(int i = 0;i<nums.length;i++)
+        {
+            if(i>maxReach) return false;
+            else{
+                maxReach = Math.max(maxReach,i+nums[i]);
+            }
+        }
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
