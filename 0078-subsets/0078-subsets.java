92f98daaa9eba4822a2c165b86a30b255975b967
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-11-03 08:10:00 +0530

    Time: 1 ms (49.06%), Space: 44.2 MB (6.46%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,24 +2,50 @@ import java.util.*;
 class Solution {
     public static void sets(List<Integer> list,List<List<Integer>> subset ,int[] nums,int i)
     {
-        if(i == nums.length)
+        if(i == -1)
         {
             subset.add(new ArrayList<>(list));
             return;
         }
         list.add(nums[i]);
-        sets(list,subset,nums,i + 1); //take it
+        sets(list,subset,nums,i - 1); //take it
         list.remove(Integer.valueOf(nums[i]));
-        sets(list,subset,nums,i + 1); //dont take it
-
+        sets(list,subset,nums,i - 1); //dont take it
     }
     public List<List<Integer>> subsets(int[] nums) {
         List<Integer> list = new ArrayList<>();
         List<List<Integer>> subset = new ArrayList<>();
-        sets(list,subset,nums,0);
+        sets(list,subset,nums,nums.length-1);
         return subset;
+    }
+}
+//from 0 to n 
+// import java.util.*;
+// class Solution {
+//     public static void sets(List<Integer> list,List<List<Integer>> subset ,int[] nums,int i)
+//     {
+//         if(i == nums.length)
+//         {
+//             subset.add(new ArrayList<>(list));
+//             return;
+//         }
+//         list.add(nums[i]);
+//         sets(list,subset,nums,i + 1); //take it
+//         list.remove(Integer.valueOf(nums[i]));
+//         sets(list,subset,nums,i + 1); //dont take it
+//     }
+//     public List<List<Integer>> subsets(int[] nums) {
+//         List<Integer> list = new ArrayList<>();
+//         List<List<Integer>> subset = new ArrayList<>();
+//         sets(list,subset,nums,0);
+//         return subset;
+//     }
+// }
 
-        // int length = nums.length;
+
+
+        // BIT MANIPULATION METHOD
+                // int length = nums.length;
         // List<List<Integer>>list = new ArrayList<>();
         // for(int j = 0;j<(1<<length);j++)
         // {
@@ -35,6 +61,4 @@ class Solution {
             
         // }
         // return list;
-        
-    }
-}
+  
