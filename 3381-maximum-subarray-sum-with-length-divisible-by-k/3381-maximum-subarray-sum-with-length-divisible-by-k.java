97f80ae1480f903b92b06f27fd7758b5599987dd
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-11-27 21:44:26 +0530

    Time: 4 ms (100%), Space: 149 MB (25.58%) - LeetHub

--- 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.java
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.java
@@ -1,39 +1,22 @@
 class Solution {
+
     public long maxSubarraySum(int[] nums, int k) {
-
         int n = nums.length;
-        long[] prefix = new long[n + 1];
-
-        // Build prefix sum
+        long prefixSum = 0;
+        long maxSum = Long.MIN_VALUE;
+        long[] kSum = new long[k];
+        for (int i = 0; i < k; i++) {
+            kSum[i] = Long.MAX_VALUE / 2;
+        }
+        kSum[k - 1] = 0;
         for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + nums[i];
+            prefixSum += nums[i];
+            maxSum = Math.max(maxSum, prefixSum - kSum[i % k]);
+            kSum[i % k] = Math.min(kSum[i % k], prefixSum);
         }
-
-        // minPrefix[r] = smallest prefix value seen with index % k == r
-        long[] minPrefix = new long[k];
-        Arrays.fill(minPrefix, Long.MAX_VALUE);
-
-        long ans = Long.MIN_VALUE;
-
-        for (int i = 0; i <= n; i++) {
-
-            int mod = i % k;
-
-            // If a previous prefix with the same mod exists,
-            // we can form a subarray whose length % k == 0
-            if (minPrefix[mod] != Long.MAX_VALUE) {
-                long candidate = prefix[i] - minPrefix[mod];
-                ans = Math.max(ans, candidate);
-            }
-
-            // Update smallest prefix for this remainder class
-            minPrefix[mod] = Math.min(minPrefix[mod], prefix[i]);
-        }
-
-        return ans;
+        return maxSum;
     }
 }
-
 //// Brute force
 // class Solution {
 //     public long maxSubarraySum(int[] nums, int k) {
