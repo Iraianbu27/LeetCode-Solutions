d02b8152ea1e5cfdc995445b574db0f8360860e9
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-12-03 12:57:01 +0530

    Time: 2 ms (84.62%), Space: 47.9 MB (5.87%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,61 +1,48 @@
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int left = 0,right=0;
+//         int n = nums.length;
+//         while(right < n)
+//         {
+//             if(nums[right] != 0)
+//             {
+//                 nums[left] = nums[right];
+//                 left++;
+//             }
+//             right++;
+//         }
+//         // left++;
+//         while(left<n)
+//         {
+//             nums[left] = 0;
+//             left++;
+//         }
+
+
+//     }
+// }
+
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left = 0,right=0;
         int n = nums.length;
-        while(right < n)
-        {
-            if(nums[right] != 0)
-            {
-                nums[left] = nums[right];
-                left++;
+        int i = 0;
+        while(i<n && nums[i] != 0){
+            i++;
+        }
+        int j=i+1;
+        while(j<n){
+            if(nums[j] != 0){
+                int thVar = nums[i];
+                nums[i] = nums[j];
+                nums[j] = thVar;
+                i++;
             }
-            right++;
-        }
-        // left++;
-        while(left<n)
-        {
-            nums[left] = 0;
-            left++;
-        }
-        // while(right < n)
-        // {
-        //     if(nums[right] != 0)
-        //     {
-        //         int thVar = nums[left];
-        //         nums[left] = nums[right];
-        //         nums[right] = thVar;
-        //         left++;
-        //     }
-        //     right++;
-        // }
-        // int n =nums.length;
-        // int j = 0;
-        // for(int i = 0;i<n;i++)
-        // {
-        //     if(nums[i] != 0)
-        //     {
-        //         nums[j++] = nums[i];
-        //         // j++;
-        //     }
+            j++;
 
-        // }
-        // while(j< nums.length)
-        // {
-        //     nums[j++] = 0;
-        //     // j++;
-        // }
-
+        }
 
     }
 }
-
-
-
-
-
-
-
-
 
 
 
