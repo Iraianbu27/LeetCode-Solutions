bc4ed8ccc05051c0138725f8b5a5b99e5d569ba3
Iraianbu27 <161296566+Iraianbu27@users.noreply.github.com>
2025-12-05 09:10:43 +0530

    Time: 2 ms (57.87%), Space: 94.4 MB (34.64%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,71 +1,58 @@
-class Solution {
-    public int maxProfit(int[] prices) {
-        int minValue = Integer.MAX_VALUE;
-        int profit = 0;
-        for(int i = 0;i<prices.length;i++)
-        {
-            if(prices[i] < minValue)
-            {
-                minValue = prices[i]; //finding the minimum value dynamicaly
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int minValue = prices[0];
+//         int profit = 0;
+//         for(int i = 1;i<prices.length;i++)
+//         {
+//             // if(prices[i] < minValue)
+//             // {
+//                 minValue = Math.min(minValue,prices[i]); //finding the minimum value dynamicaly
+//             // }
+//             // else if(prices[i] - minValue > profit)
+//             // {
+//                 profit = Math.max(profit,prices[i] - minValue);
+//             // }
+//         }
+//         return profit;
+//     }
+// }
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int minValue = Integer.MAX_VALUE;
+//         int profit = 0;
+//         for(int i = 0;i<prices.length;i++)
+//         {
+//             if(prices[i] < minValue)
+//             {
+//                 minValue = prices[i]; //finding the minimum value dynamicaly
+//             }
+//             else if(prices[i] - minValue > profit)
+//             {
+//                 profit = prices[i] - minValue;
+//             }
+//         }
+//         return profit;
+//     }
+// }
+
+class Solution{
+    public int maxProfit(int[] prices){
+        int n = prices.length;
+        int buy = prices[0];
+        int maxProfit = 0;
+        for(int i=1;i<n;i++){
+            
+            if(prices[i]<buy){
+                buy = prices[i];
             }
-            else if(prices[i] - minValue > profit)
-            {
-                profit = prices[i] - minValue;
+            else if(prices[i]-buy > maxProfit){
+                maxProfit = prices[i]-buy;
             }
+            // else{
+            //     int profit = prices[i]-buy;
+            //     maxProfit = Math.max(maxProfit,profit);
+            // }
         }
-        return profit;
+        return maxProfit;
+    }
 }
-}
-        //int min = Integer.MAX_VALUE;
-        // int max = -1;
-        // int maxIndex=0;
-        // int minIndex=0;
-        // for(int i = 0;i<prices.length;i++)
-        // {
-        //    if(min>prices[i])
-        //    {
-        //     min = prices[i];
-        //     minIndex = i;
-        //    }
-        // }
-         
-        // if(minIndex == (prices.length - 1)){
-        //     return 0;
-        // }
-        // // else if((prices.length == 2) && (minIndex == 0))
-        // // {
-        // //     return prices[1]-prices[0];
-        // // }
-        // else
-        // {
-        //     for(int i = minIndex + 1;i<prices.length;i++)
-        //     {
-        //          if(max < prices[i])
-        //             {
-        //                 max = prices[i];
-        //                 maxIndex = i;
-
-        //             }
-        //     }
-        // }
-        // return max-min;
-        // int max=0,min = Integer.MAX_VALUE;
-        // int maxIndex = 0,minIndex = 0;
-        // for(int i=0;i<prices.length;i++)
-        // {
-        //     if(max < prices[i])
-        //     {
-        //         max = prices[i];
-        //         maxIndex = i;
-
-        //     }
-        //     if(min >prices[i]){
-        //         min = prices[i];
-        //         minIndex = i;
-        //     }
-        // }
-        // if(minIndex == (prices.length - 1))
-        // {
-        //     return 0;
-        // }
-        // return max-min;
